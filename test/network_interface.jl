33a7ea318d818eb5edcb7858ae076226fe7a0f82
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2021-06-21 11:35:47 +0200

    fuse_projectors another examples

--- test/network_interface.jl
+++ test/network_interface.jl
@@ -1,7 +1,34 @@
-@testset "fuse_projectors correctly fuses projectors." begin
+@testset "fuse_projectors correctly fuses projectors - 2x2 matrix." begin
     projectors = ([1 1; 0 1], [1 0; 1 1])
     expected_fused = [1 0; 0 1]
     expected_transitions = [[1 1; 0 1], [1 0; 1 1]]
+    fused, transitions = fuse_projectors(projectors)
+    @test expected_fused == fused
+    @test expected_transitions == values(transitions)
+end
+
+@testset "fuse_projectors correctly fuses projectors - another 2x2 matrix." begin
+    projectors = ([1 1; 0 0], [1 0; 0 1])
+    expected_fused = [1 0; 0 1]
+    expected_transitions = [[1 1; 0 0], [1 0; 0 1]]
+    fused, transitions = fuse_projectors(projectors)
+    @test expected_fused == fused
+    @test expected_transitions == values(transitions)
+end
+
+@testset "fuse_projectors correctly fuses projectors - 3x3 matrix." begin
+    projectors = ([1 1 1; 0 0 0; 1 1 1], [1 0 0; 0 1 0; 0 0 1])
+    expected_fused = [1 0 0; 0 1 0; 0 0 1]
+    expected_transitions = [[1 1 1; 0 0 0; 1 1 1], [1 0 0; 0 1 0; 0 0 1]]
+    fused, transitions = fuse_projectors(projectors)
+    @test expected_fused == fused
+    @test expected_transitions == values(transitions)
+end
+
+@testset "fuse_projectors correctly fuses projectors - another 3x3 matrix." begin
+    projectors = ([1 1 1; 0 0 0; 1 1 0], [1 0 1; 0 1 0; 0 0 1])
+    expected_fused = [1 0 0; 0 1 0; 0 0 1]
+    expected_transitions = [[1 1 1; 0 0 0; 1 1 0], [1 0 1; 0 1 0; 0 0 1]]
     fused, transitions = fuse_projectors(projectors)
     @test expected_fused == fused
     @test expected_transitions == values(transitions)

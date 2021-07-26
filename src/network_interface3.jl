8e31734bb2955f579711eef268a1e191cd25e4b4
bartek gardas <bartek.gardas@gmail.com>
2021-07-26 15:34:40 +0200

    fixing  generate_boundary_state_with_fusing

--- src/network_interface3.jl
+++ src/network_interface3.jl
@@ -3,47 +3,13 @@ using LabelledGraphs
 export fuse_projectors, build_tensor_with_fusing, generate_boundary_states_with_fusing
 
 
-#function fuse_projectors(projectors)
-#    fused, energy = rank_reveal(hcat(projectors...), :PE)
-#    i₀ = 1
-#    transitions = []
-#    for proj ∈ projectors
-#        iₑ = i₀ + size(proj, 2) - 1
-#        push!(transitions, energy[:, i₀:iₑ])
-#        i₀ = iₑ + 1
-#    end
-#    fused, transitions
-#end
-
-
-# This has to be unified with build_tensor
-#@memoize function build_tensor_with_fusing(network::AbstractGibbsNetwork{S, T}, v::S) where {S, T}
-#    loc_exp = exp.(-network.β .* local_energy(network, v))
-
-    #(pl, pt, pr, pb), trl, trr
-#    projs, trl, trr = projectors_with_fusing(network, v) # only difference in comparison to build_tensor
-#    dim = zeros(Int, length(projs))
-#    @cast A[_, i] := loc_exp[i]
-    #v = build_tensor(peps, (i-1, j), (i, j)) ###
-    #@tensor A[l, u, r, d] := v[u, ũ] * A[l, ũ, r, d] ###
-
-#    for (j, pv) ∈ enumerate(projs)
-#        @cast A[(c, γ), σ] |= A[c, σ] * pv[σ, γ]
-#        dim[j] = size(pv, 2)
-#    end
-#    reshape(A, dim..., :), trl, trr 
-#end
-
-
 function generate_boundary_state_with_fusing(network::NNNNetwork, v::S, w::S, σ::Vector{Int}) where {S, T}
     v_lin = node_index_with_fusing(network, v)
     state =  0 < v_lin <= length(σ) ? σ[v_lin] : 1
     if v ∉ vertices(network.network_graph) return ones_like(state) end
-    loc_dim = length(local_energy(network, v))
     pv = projector(network, v, w)
     [findfirst(x -> x > 0, pv[i, :]) for i ∈ 1:size(pv)[1]][state]
 end
-
 
 function generate_boundary_state_with_fusing(network::NNNNetwork, v::S, w::S, k::S, σ::Vector{Int}) where {S, T}
     v_lin = node_index_with_fusing(network, v)
@@ -87,7 +53,7 @@ function generate_boundary_states_with_fusing(
     node::S
 ) where {S, T}
     [
-        generate_boundary_state_with_fusing(network, x..., σ)
+        generate_boundary_state_with_fusing.(Ref(network), Ref(x)..., σ)
         for x ∈ boundary_at_splitting_node(network, node)
     ]
 end

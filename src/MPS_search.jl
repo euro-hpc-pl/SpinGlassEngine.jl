da02c77ac4d80b357c4bd8f19ff2b62cc33d6142
bartek gardas <bartek.gardas@gmail.com>
2021-08-10 16:34:23 +0200

    rm LabelledGraphs from MPS_search.jl

--- src/MPS_search.jl
+++ src/MPS_search.jl
@@ -1,9 +1,8 @@
-using LabelledGraphs
-
 export 
     solve,
     low_energy_spectrum
 
+    
 _make_left_env(ψ::AbstractMPS, k::Int) = ones(eltype(ψ), 1, k)
 
 _make_LL(ψ::AbstractMPS, b::Int, k::Int, d::Int) = zeros(eltype(ψ), b, k, d)

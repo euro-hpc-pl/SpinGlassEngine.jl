81363181fcb182fc8f97a68fdef79c082dce42b7
bartek gardas <bartek.gardas@gmail.com>
2021-08-27 16:15:00 +0200

    ready for testing

--- test/search_chimera2048.jl
+++ test/search_chimera2048.jl
@@ -1,17 +1,17 @@
-using Memoize
-@testset "Chimera 2048 instance has the correct low energy spectrum" begin
-    m = 8 
-    n = 8
+
+#@testset "Chimera 2048 instance has the correct low energy spectrum" begin
+    m = 4 
+    n = 4
     t = 8
 
     β = 1.
 
     L = n * m * t
-    num_states = 5
+    num_states = 100
 
     #instance = "$(@__DIR__)/instances/chimera_droplets/2048power/001.txt"
-    instance = "$(@__DIR__)/instances/chimera_droplets/512power/001.txt"
-    #instance = "$(@__DIR__)/instances/chimera_droplets/128power/001.txt"
+    #instance = "$(@__DIR__)/instances/chimera_droplets/512power/001.txt"
+    instance = "$(@__DIR__)/instances/chimera_droplets/128power/001.txt"
 
     ig = ising_graph(instance)
 
@@ -25,10 +25,10 @@ using Memoize
     for transform ∈ rotation.([0])
         peps = PEPSNetwork(m, n, fg, transform, β=β, bond_dim=32)
 
-        # @time x = mps(peps, 1)
+        #@time x = mps(peps, 1)
 
         #update_gauges!(peps, :rand)
         @time sol = low_energy_spectrum(peps, num_states)#, merge_branches(peps, 1.0))
-        #println(sol.energies[1:1])
+        println(sol.energies[1:1])
     end
-end
+#end

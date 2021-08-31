ff4427454a250579a893fb413c9ae09b3122d0a6
bartek gardas <bartek.gardas@gmail.com>
2021-08-31 15:04:55 +0200

    clean up

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,51 +9,18 @@ using MetaGraphs
 disable_logging(LogLevel(1))
 
 using Test
-
-function proj(state, dims::Union{Vector, NTuple})
-    P = Matrix{Float64}[]
-    for (σ, r) ∈ zip(state, dims)
-        v = zeros(r)
-        v[idx(σ)...] = 1.
-        push!(P, v * v')
-    end
-    P
-end
-
-# multiple dispatch with functions in network_tensors.jl doesn't work
-#=
-function tensor(ψ::AbstractMPS, state::State)
-    C = I
-    for (A, σ) ∈ zip(ψ, state)
-        C *= A[:, idx(σ), :]
-    end
-    tr(C)
-end
-
-function tensor(ψ::MPS)
-    dims = rank(ψ)
-    Θ = Array{eltype(ψ)}(undef, dims)
-
-    for σ ∈ all_states(dims)
-        Θ[idx.(σ)...] = tensor(ψ, σ)
-    end
-    Θ
-end
-=#
-
 my_tests = []
 
 push!(my_tests,
-        #"network_operations.jl",
-        #"branch_and_bound.jl",
-        #"network_interface.jl",
-        #"ising_MPS.jl",
-        #"search_MPS.jl",
-        #"search_chimera.jl",
-        #"search_cross.jl",
-       "search_chimera2048.jl",
-       #"mps_mpo_times_test.jl"
-        #"network_tensors.jl",
+        "network_operations.jl",
+        "branch_and_bound.jl",
+        "network_interface.jl",
+        "ising_MPS.jl",
+        "search_MPS.jl",
+        "search_chimera.jl",
+        "search_cross.jl",
+        "network_tensors.jl",
+        "search_chimera2048.jl",
 )
 
 for my_test in my_tests

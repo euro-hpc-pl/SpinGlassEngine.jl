77c054ae253786a47902bbafe0208d945174332f
Marek Rams <marek.rams@uj.edu.pl>
2021-09-15 12:48:00 +0200

    cleaning compress!

--- src/compressions.jl
+++ src/compressions.jl
@@ -1,3 +1,5 @@
+export compress!
+
 abstract type AbstractEnvironment end
 
 mutable struct Environment <: AbstractEnvironment
@@ -15,56 +17,72 @@ mutable struct Environment <: AbstractEnvironment
         ket::Dict,
         transpose::Bool = false
     )
-        sites_bra = sort(keys(bra))
-        sites_mpo = sort(keys(mpo))
-        sites_ket = sort(keys(ket))
+        sites_bra = sort(collect(keys(bra)))
+        sites_mpo = sort(collect(keys(mpo)))
+        sites_ket = sort(collect(keys(ket)))
 
         @assert sites_bra == sites_ket
-        @assert issubset(sites_bra, site_mpo)  
+        @assert issubset(sites_bra, sites_mpo)
 
         # mpo can have sites with indices that are not in bra and ket
         env = Dict(
                    (first(sites_bra), :left) => ones(1, 1, 1),
                    (last(sites_bra), :right) => ones(1, 1, 1)
             )
-        env = new(bra, mpo, ket, transpose, sites_bra, sites_mpo, env)
-        for site ∈ env.sites_bra update_env!(env, site, :left) end
-        env
+        environment = new(bra, mpo, ket, transpose, sites_bra, sites_mpo, env)
+        for site ∈ environment.sites_bra update_env_left!(environment, site) end
+        environment
     end
 end
 
 
-function SpinGlassTensors.compress(
-    W::Dict, 
-    ψ::Dict, 
+function compress!(
+    bra::Dict,
+    mpo::Dict,
+    ket::Dict,
     Dcut::Int,
-    ψ₀::Dict, 
     tol::Number=1E-8,
     max_sweeps::Int=4
 )
-    # initial guess + its canonization
-    # ψ₀ :rand, :svd, Dict(mps)
-    ket = copy(ψ)
 
-    env = Environment(ψ, W, ket, true)
+    env = Environment(bra, mpo, ket, true)
     overlap_before = measure_env(env, last(env.sites_bra))
 
     for sweep ∈ 1:max_sweeps
-        _left_sweep_var!(env, Dcut)
-        _right_sweep_var!(env, Dcut)
+        for site ∈ reverse(env.sites_bra)
+            update_env_right!(env, site)
+            T = project_ket_on_bra(env, site)
+            d = size(T, 2)
+            @cast T2[x, (y, z)] := T[x, y, z]
+            Q = rq(T2, Dcut)
+            @cast T3[x, σ, y] |= Q[x, (σ, y)] (σ ∈ 1:d)
+            env.bra[site] = T3
+            clear_env_site!(env, site)
+        end
+        for site ∈ env.sites_bra
+            update_env_left!(env, site)
+            T = project_ket_on_bra(env, site)
+            d = size(T, 2)
+            @cast T2[(x, y), z] := T[x, y, z]
+            Q = qr(T2, Dcut)
+            @cast T3[x, σ, y] |= Q[(x, σ), y] (σ ∈ 1:d)
+            env.bra[site] = T3
+            clear_env_site!(env, site)
+        end
+
         overlap = measure_env(env, last(env.sites_bra))
 
         Δ = abs(overlap_before - abs(overlap))
         @info "Convergence" Δ
+        println("Convergence", " ", sweep, " " Δ)
 
         if Δ < tol
             @info "Finished in $sweep sweeps of $(max_sweeps)."
-            return ϕ
+            return
         else
             overlap_before = overlap
         end
     end
-    ϕ
 end
 
 
@@ -74,23 +92,23 @@ end
 # maximum(filter(x -> x < site, sites))
 function _neighbouring_site_to_left(site, sites)
     # largest x in sites: x < site
-    last = first(sites)
+    ind = first(sites)
     for x ∈ sites
-        if x >= sites break end
-        last = x
+        if x >= site break end
+        ind = x
     end
-    last  # what should it return if there  is nothigh to the left?
+    ind  # what should it return if there  is nothigh to the left?
 end
 
 # minimum(filter(x -> x > site, sites))
 function _neighbouring_site_to_right(site, sites)
     # smallest x in sites: x > site
-    last = last(sites)
-    for x ∈ reversed(sites)
-        if x <= sites break end
-        last = x
+    ind = last(sites)
+    for x ∈ reverse(sites)
+        if x <= site break end
+        ind = x
     end
-    last  # what should it return if there  is nothigh to the right
+    ind  # what should it return if there  is nothigh to the right
 end
 
 
@@ -98,51 +116,51 @@ end
 function update_env_left!(env, site)
     if site <= first(env.sites_bra) return end
 
-    lsite = _neighbour_site_to_left(site, env.sites_bra)
+    lsite = _neighbouring_site_to_left(site, env.sites_bra)
     LE = env.env[(lsite, :left)]
     T = env.bra[lsite]
     M = env.mpo[lsite]
     B = env.ket[lsite]
-    Lnew = update_left_env(LE, T, M, B)
+    Lnew = update_env_left(LE, T, M, B)
 
-    isite = _neighbour_site_to_right(lsite, env.sites_mpo)
+    isite = _neighbouring_site_to_right(lsite, env.sites_mpo)
     while isite < site
         iM = env.mpo[isite]
-        Lnew = update_left_env(Lnew, iM)
-        isite = _neighbour_site_to_right(isite, env.sites_mpo)
+        Lnew = update_env_left(Lnew, iM)
+        isite = _neighbouring_site_to_right(isite, env.sites_mpo)
     end
-    push!(env.env, (r, :left) => Lnew)
+    push!(env.env, (site, :left) => Lnew)
 end
 
 
-function update_right_env!(env, site)
+function update_env_right!(env, site)
     if site >= last(env.sites_bra) return end
 
-    rsite = _neighbour_site_to_right(site, env.sites_bra)
+    rsite = _neighbouring_site_to_right(site, env.sites_bra)
     RE = env.env[(rsite, :right)]
     T = env.bra[rsite]
     M = env.mpo[rsite]
     B = env.ket[rsite]
-    Rnew = update_right_env(RE, T, M, B)
+    Rnew = update_env_right(RE, T, M, B)
 
-    isite = _neighbour_site_to_left(rsite, env.sites_mpo)
+    isite = _neighbouring_site_to_left(rsite, env.sites_mpo)
     while isite > site
         iM = env.mpo[isite]
-        Rnew = update_right_env(Rnew, iM)
-        isite = _neighbour_site_to_left(isite, env.sites_mpo)
+        Rnew = update_env_right(Rnew, iM)
+        isite = _neighbouring_site_to_left(isite, env.sites_mpo)
     end
-    push!(env.env, (r, :right) => Rnew)
+    push!(env.env, (site, :right) => Rnew)
 end
 
 
 function clear_env_site!(env, site)
-    delate!(env.env, (site, :right))
-    delate!(env.env, (site, :left))
+    # delete!(env.env, (site, :right))
+    # delete!(env.env, (site, :left))
 end
 
 
 
-function update_left_env(LE, T, M, B)  # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
+function update_env_left(LE, T, M, B)  # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
     @tensor L[nb, nc, nt] := LE[ob, oc, ot] * T[ot, α, nt] * 
                              M[oc, α, nc, β] * B[ob, β, nb] order = (ot, α, oc, β, ob)  
     # for real there is no conjugate, otherwise conj(T)
@@ -151,7 +169,7 @@ end
 
 
 #=
-function update_left_env(LE, T, M, B, :transposed)  # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
+function update_env_left(LE, T, M, B, :transposed)  # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
     @tensor L[nb, nc, nt] := LE[ob, oc, ot] * T[ot, α, nt] * 
                              M[oc, β, nc, α] * B[ob, β, nb] order = (ot, α, oc, β, ob)  
     # for real there is no conjugate, otherwise conj(T)
@@ -159,28 +177,28 @@ function update_left_env(LE, T, M, B, :transposed)  # same tensory bez wymiernyc
 end
 =#
 
-function update_left_env(LE, iM)  # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
-    @tensor L[nb, nc, nt] := LE[nb, oc, nt] * M[oc, nc]
+function update_env_left(LE, iM)  # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
+    @tensor L[nb, nc, nt] := LE[nb, oc, nt] * iM[oc, nc]
     L
 end
 
 
-function update_right_env(RE, T, M, B)  # same tensory bez wymiernych indeksow
+function update_env_right(RE, T, M, B)  # same tensory bez wymiernych indeksow
     @tensor R[nt, nc, nb] := RE[ot, oc, ob] * T[nt, α, ot] * M[nc, α, oc, β] * B[nb, β, ob] order = (ot, α, oc, β, ob)
     # for real there is no conjugate, otherwise conj(T)
     R
 end
 
 #=
-function update_right_env(RE, T, M, B, :transposed)  # same tensory bez wymiernych indeksow
+function update_env_right(RE, T, M, B, :transposed)  # same tensory bez wymiernych indeksow
     @tensor R[nt, nc, nb] := RE[ot, oc, ob] * T[nt, α, ot] * M[nc,  β, oc, α] * B[nb, β, ob] order = (ot, α, oc, β, ob)
     # for real there is no conjugate, otherwise conj(T)
     R
 end
 =#
 
-function update_right_env(RE, iM)  # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
-    @tensor R[nt, nc, nb] := M[nc, oc] * RE[nt, oc, nb]
+function update_env_right(RE, iM)  # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
+    @tensor R[nt, nc, nb] := iM[nc, oc] * RE[nt, oc, nb]
     R
 end
 
@@ -214,38 +232,13 @@ function measure_env(env, site)
     M = env.mpo[site]
     B = env.ket[site]
     RE = env.env[(site, :right)]
-    LL = update_left_env(LE, T, M, B)
+    LL = update_env_left(LE, T, M, B)
     @tensor scalar = LL[t, c, b] * RE[b, c, t]
     scalar
 end
 
 
-function _left_sweep_var!(env)
-    for r ∈ reversed(env.sites_bra)
-        update_right_env!(env, site)
-        T = project_ket_on_bra(env, site)
-        d = size(T, 2)
-        @cast T2[x, (y, z)] := T[x, y, z]
-        Q = rq(T2, Dcut)
-        @cast T3[x, σ, y] |= Q[x, (σ, y)] (σ ∈ 1:d)
-        env.bra[site] = T3
-        clear_env_site!(env, site)
-    end
-end
 
-
-function _right_sweep_var!(env)
-    for r ∈ env.sites_bra
-        update_left_env!(env, site)
-        T = project_ket_on_bra(env, site)
-        d = size(T, 2)
-        @cast T2[(x, y), z] := T[x, y, z]
-        Q = qr(T2, Dcut)
-        @cast T3[x, σ, y] |= Q[(x, σ), y] (σ ∈ 1:d)
-        env.bra[site] = T3
-        clear_env_site!(env, site)
-    end
-end
 
 
 # function canonise!(ψ::AbstractMPS)
--- test/compressions.jl
+++ test/compressions.jl
@@ -1,18 +1,44 @@
 @testset "Compressions for sparse mps and mpo works" begin
     D = 16
     d = 2
-    sites = 5
+    sites = 20
     T = Float64
     
     Dcut = 8 
-    max_sweeps = 4
-    tol = 1E-8
+    max_sweeps = 30
+    tol = 1E-20
 
     ψ = randn(MPS{T}, sites, D, d)
     W = randn(MPO{T}, sites, D, d)
 
-    mps = Dict(i => A for (i, A) ∈ enumerate(ψ))
+    
+    ket = Dict(i => A for (i, A) ∈ enumerate(ψ))
     mpo = Dict(i => O for (i, O) ∈ enumerate(W))
 
-    ϕ = compress(W, ψ, Dcut, ψ₀, tol, max_sweeps)
+    # ψ₀
+    @time ψ_new = compress(W * ψ, Dcut, tol, 1000)
+
+    bra = Dict(i => A for (i, A) ∈ enumerate(ψ_new))
+    
+    @time compress!(bra, mpo, ket, Dcut, tol, max_sweeps)
+
+    M = randn(MPS{T}, sites, D, d)
+    for i ∈ 1:sites
+        M[i] = bra[i]
+    end
+
+    println(ψ_new * ψ_new)
+    println(M * M)
+    println(M * ψ_new)
+    println(ψ_new * M)
+
+    xx = W * ψ
+
+    println(xx * ψ_new)
+    println(xx * M)
+    
+
+    # println(ψ_new)
+    # println(M)
+
 end

72d7ae43bf301824a9e6642274c049f88c783e74
bartekGardas <bartek.gardas@gmail.com>
2021-09-16 19:18:04 +0200

    clean up example

--- test/compressions.jl
+++ test/compressions.jl
@@ -1,3 +1,15 @@
+function SpinGlassTensors.MPS(ket::Dict)
+    L = length(ket)
+    ϕ = MPS(eltype(ket[1]), L) 
+    for i ∈ 1:L ϕ[i] = ket[i] end
+    ϕ
+end
+
+Base.Dict(
+    ϕ::SpinGlassTensors.AbstractTensorNetwork
+) = Dict(i => A for (i, A) ∈ enumerate(ϕ))
+
+
 @testset "Compressions for sparse mps and mpo works" begin
     D = 16
     d = 2
@@ -5,31 +17,27 @@
     T = Float64
     
     Dcut = 8
-    max_sweeps = 30
+    max_sweeps = 10
     tol = 1E-10
 
     ψ = randn(MPS{T}, sites, D, d)
     W = randn(MPO{T}, sites, D, d)
 
-    ket = Dict(i => copy(A) for (i, A) ∈ enumerate(ψ))
-    mpo = Dict(i => B for (i, B) ∈ enumerate(W))
+    ket = Dict(ψ)
+    mpo = Dict(W)
 
     @testset "Two mps representations are compressed to the same state" begin 
-        @time ψ̃  = compress(W * ψ, Dcut, tol, max_sweeps)
+        @time χ = compress(W * ψ, Dcut, tol, max_sweeps)
 
-        bra = copy(ket)
-        ϕ = MPS(T, sites) 
-        for i ∈ 1:sites ϕ[i] = copy(ket[i]) end
-        canonise!(ϕ, :right)
-        bra = Dict(i => copy(A) for (i, A) ∈ enumerate(ϕ))
+        #ϕ = copy(ψ)
+        #canonise!(ϕ, :right)
+        #bra = Dict(ϕ)
 
-        #bra = Dict(i => copy(A) for (i, A) ∈ enumerate(ψ̃ ))
+        bra = copy(Dict(χ))
         @time compress!(bra, mpo, ket, Dcut, tol, max_sweeps)
 
-        ϕ = MPS(T, sites) 
-        for i ∈ 1:sites ϕ[i] = bra[i] end
-
-        @test ψ̃ * ψ̃ ≈ ϕ * ϕ ≈ 1
-        @test dot(ϕ, ψ̃ ) ≈ dot(ψ̃ , ϕ) ≈ 1 
+        ϕ = MPS(bra)
+        @test χ * χ ≈ ϕ * ϕ ≈ 1
+        @test dot(ϕ, χ) ≈ dot(χ, ϕ) ≈ 1 
     end
 end

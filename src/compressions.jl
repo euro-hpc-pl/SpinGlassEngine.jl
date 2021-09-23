5a00df5f8d02d5c06febf677f12bd310171e7956
bartekGardas <bartek.gardas@gmail.com>
2021-09-23 21:22:09 +0200

    simplify logic

--- src/compressions.jl
+++ src/compressions.jl
@@ -23,30 +23,27 @@ mutable struct Environment <: AbstractEnvironment
     mpo::Dict
     ket::Dict
     transpose::Bool
-    sites_bra
-    sites_mpo
     env::Dict
 
     function Environment(
         bra::Dict,
         mpo::Dict,
         ket::Dict,
-        transpose::Bool = false
+        trans::Symbol=:c
     )
-        sites_bra = sort(collect(keys(bra)))
-        sites_mpo = sort(collect(keys(mpo)))
-        sites_ket = sort(collect(keys(ket)))
-
-        @assert sites_bra == sites_ket
-        @assert issubset(sites_bra, sites_mpo)
+        @assert trans ∈ (:n, :c)
+        @assert bra.sites == ket.sites
+        @assert issubset(bra.sites, mpo.sites)
 
         # mpo can have sites with indices that are not in bra and ket
         env = Dict(
-                   (first(sites_bra), :left) => ones(1, 1, 1),
-                   (last(sites_bra), :right) => ones(1, 1, 1)
+                   (first(bra.sites), :left) => ones(1, 1, 1),
+                   (last(bra.sites), :right) => ones(1, 1, 1)
             )
-        environment = new(bra, mpo, ket, transpose, sites_bra, sites_mpo, env)
-        for site ∈ environment.sites_bra update_env_left!(environment, site) end
+        environment = new(bra, mpo, ket, trans, env)
+        for site ∈ environment.bra.sites 
+            update_env_left!(environment, site) 
+        end
         environment
     end
 end
@@ -61,7 +58,7 @@ function SpinGlassTensors.compress!(
     max_sweeps::Int=4,
     args...
 )
-    env = Environment(bra, mpo, ket, true)
+    env = Environment(bra, mpo, ket)
     overlap = Inf
     overlap_before = measure_env(env, last(env.sites_bra))
 
@@ -86,7 +83,7 @@ end
 
 
 function _left_sweep_var!(env::Environment, args...)
-    for site ∈ reverse(env.sites_bra)
+    for site ∈ reverse(env.bra.sites)
         update_env_right!(env, site)
         A = project_ket_on_bra(env, site)
         @cast B[x, (y, z)] := A[x, y, z]
@@ -99,7 +96,7 @@ end
 
 
 function _right_sweep_var!(env::Environment, args...)
-    for site ∈ env.sites_bra
+    for site ∈ env.bra.sites
         update_env_left!(env, site)
         A = project_ket_on_bra(env, site)
         @cast B[(x, y), z] := A[x, y, z]
@@ -111,43 +108,39 @@ function _right_sweep_var!(env::Environment, args...)
 end
 
 
-function _neighbouring_site_to_left(site, sites)
+function _left_nbrs_site(site, sites)
     # largest x in sites: x < site
-    ind = -Inf
-    for x ∈ sites
-        if x >= site break end
-        ind = x
-    end
-    ind  
+    ls = filter(i -> i < site, sites)
+    if isempty(ls) return -Inf end
+    maximum(ls)
 end
 
 
-function _neighbouring_site_to_right(site, sites)
+function _right_nbrs_site(site, sites)
     # smallest x in sites: x > site
-    ind = Inf
-    for x ∈ reverse(sites)
-        if x <= site break end
-        ind = x
-    end
-    ind  
+    ms = filter(i -> i > site, sites)
+    if isempty(ms) return Inf
+    minimum(ms)
 end
 
 
 function update_env_left!(env::Environment, site)
-    if site <= first(env.sites_bra) return end
+    if site <= first(env.bra.sites) return end
 
-    lsite = _neighbouring_site_to_left(site, env.sites_bra)
-    LE = env.env[(lsite, :left)]
-    T = env.bra[lsite]
-    M = env.mpo[lsite]
-    B = env.ket[lsite]
-    Lnew = update_env_left(LE, T, M, B)
+    lsite = _left_nbrs_site(site, env.bra.sites)
+    Lnew = update_env_left(
+                env.env[(lsite, :left)],
+                env.bra[lsite],
+                env.mpo[lsite],
+                env.ket[lsite],
+    )
 
-    isite = _neighbouring_site_to_right(lsite, env.sites_mpo)
+    isite = _right_nbrs_site(lsite, env.mpo.sites)
+
     while isite < site
         iM = env.mpo[isite]
         Lnew = update_env_left(Lnew, iM)
-        isite = _neighbouring_site_to_right(isite, env.sites_mpo)
+        isite = _right_nbrs_site(isite, env.mpo.sites)
     end
     push!(env.env, (site, :left) => Lnew)
 end
@@ -156,18 +149,19 @@ end
 function update_env_right!(env::Environment, site)
     if site >= last(env.sites_bra) return end
 
-    rsite = _neighbouring_site_to_right(site, env.sites_bra)
-    RE = env.env[(rsite, :right)]
-    T = env.bra[rsite]
-    M = env.mpo[rsite]
-    B = env.ket[rsite]
-    Rnew = update_env_right(RE, T, M, B)
+    rsite = _right_nbrs_site(site, env.bra.sites)
+    Rnew = update_env_right(  
+          RE = env.env[(rsite, :right)]
+          T = env.bra[rsite]
+          M = env.mpo[rsite]
+          B = env.ket[rsite]
+    )
 
-    isite = _neighbouring_site_to_left(rsite, env.sites_mpo)
+    isite = _left_nbrs_site(rsite, env.mpo.sites)
     while isite > site
         iM = env.mpo[isite]
         Rnew = update_env_right(Rnew, iM)
-        isite = _neighbouring_site_to_left(isite, env.sites_mpo)
+        isite = _left_nbrs_site(isite, env.mpo.sites)
     end
     push!(env.env, (site, :right) => Rnew)
 end
@@ -187,24 +181,31 @@ function update_env_left(LE::S, T::S, M::S, B::S) where S <: AbstractMatrix
 end
 
 
-function update_env_left(LE::S, T0::S, M::Dict, B0::S) where S <: AbstractMatrix 
-    ver_sites = collect(sort(keys(M)))
-
-    T = T0
-    for i ∈ ver_sites
+function _update_tensor_forward!(T::S, M::Dict, sites) where S <: AbstractMatrix 
+    for i ∈ sites
         if i == 0 break end
         A = M[i]
         @tensor T[l, x, r] := T[l, y, r] * A[y, x]
     end
+end
 
-    B = B0
-    for i ∈ reverse(ver_sites)
+
+function _update_tensor_backwards!(T::S, M::Dict, sites) where S <: AbstractMatrix 
+    for i ∈ reverse(sites)
         if i == 0 break end
         A = M[i]
-        @tensor B[l, x, r] := B[l, y, r] * A[x, y]
+        @tensor T[l, x, r] := T[l, y, r] * A[x, y]
     end
-    update_env_left(LE, T, M[0], B)
 end
+
+
+function update_env_left(LE::S, T₀::S, M::Dict, B₀::S) where S <: AbstractMatrix 
+    sites = collect(sort(keys(M)))
+    _update_tensor_forward!(T₀, M, sites)
+    _update_tensor_backwards!(B₀, M, sites)
+    update_env_left(LE, T₀, M[0], B₀)
+end
+
 
 #=
 function update_env_left(LE, T, M, B, :transposed)  # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
@@ -215,36 +216,23 @@ function update_env_left(LE, T, M, B, :transposed)  # same tensory bez wymiernyc
 end
 =#
 
-function update_env_left(LE::S, iM::S) where S <: AbstractMatrix # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
-    @tensor L[nb, nc, nt] := LE[nb, oc, nt] * iM[oc, nc]
+function update_env_left(LE::S, iM::S) where S <: AbstractMatrix 
     L
 end
 
 
-function update_env_right(RE::S, T::S, M::S, B::S)  where S <: AbstractMatrix # same tensory bez wymiernych indeksow
+function update_env_right(RE::S, T::S, M::S, B::S)  where S <: AbstractMatrix #
     @tensor R[nt, nc, nb] := RE[ot, oc, ob] * T[nt, α, ot] * M[nc, α, oc, β] * B[nb, β, ob] order = (ot, α, oc, β, ob)
     # for real there is no conjugate, otherwise conj(T)
     R
 end
 
 
-function update_env_right(RE::S, T0::S, M::Dict, B0::S) where S <: AbstractMatrix # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
-    ver_sites = collect(sort(keys(M)))
- 
-    T = T0
-    for i in ver_sites
-        if i == 0 break end
-        A = M[i]
-        @tensor T[l, x, r] := T[l, y, r] * A[y, x]
-    end
-
-    B = B0
-    for i in reverse(ver_sites)
-        if i == 0 break end
-        A = M[i]
-        @tensor B[l, x, r] := B[l, y, r] * A[x, y]
-    end
-    update_env_right(RE, T, M[0], B)
+function update_env_right(RE::S, T₀::S, M::Dict, B₀::S) where S <: AbstractMatrix 
+    sites = collect(sort(keys(M)))
+    _update_tensor_forward!(T₀, M, sites)
+    _update_tensor_backwards!(B₀, M, sites)
+    update_env_right(RE, T₀, M[0], B₀)
 end
 
 #=
@@ -255,18 +243,19 @@ function update_env_right(RE, T, M, B, :transposed)  # same tensory bez wymierny
 end
 =#
 
-function update_env_right(RE::S, iM::S) where S <: AbstractMatrix # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
+function update_env_right(RE::S, iM::S) where S <: AbstractMatrix 
     @tensor R[nt, nc, nb] := iM[nc, oc] * RE[nt, oc, nb]
     R
 end
 
 
 function project_ket_on_bra(env::Environment, site)
-    LE = env.env[(site, :left)]
-    M = env.mpo[site]
-    B = env.ket[site]
-    RE = env.env[(site, :right)]
-    project_ket_on_bra(LE, B, M, RE)
+    project_ket_on_bra(
+        env.env[(site, :left)]
+        env.ket[site]
+        env.mpo[site]
+        env.env[(site, :right)]
+    )
 end
 
 
@@ -276,22 +265,12 @@ function project_ket_on_bra(LE::S, B::S, M::S, RE::S) where S <: AbstractMatrix
     T
 end
 
-function project_ket_on_bra(LE::S, B0::S, M::Dict, RE::S) where S <: AbstractMatrix
-    ver_sites = collect(sort(keys(M)))
 
-    B = B0
-    for i in reverse(ver_sites)
-        if i == 0 break end
-        A = M[i]
-        @tensor B[l, x, r] := B[l, y, r] * A[x, y]
-    end
-
-    T = project_ket_on_bra(LE, B, M[0], RE)
-    for i in ver_sites
-        if i == 0 break end
-        A = M[i]
-        @tensor T[l, x, r] := T[l, y, r] * A[x, y]
-    end
+function project_ket_on_bra(LE::S, B₀::S, M::Dict, RE::S) where S <: AbstractMatrix
+    sites = collect(sort(keys(M)))
+    _update_tensor_forward!(B₀, M, sites)
+    T = project_ket_on_bra(LE, B₀, M[0], RE)
+    _update_tensor_backwards(T, M, sites)
     T
 end
 
@@ -304,28 +283,14 @@ end
 =#
 
 function measure_env(env::Environment, site)
+    LL = update_env_left(
+        env.env[(site, :left)]
+        env.bra[site]
+        env.mpo[site]
+        env.ket[site]
+    )
     LE = env.env[(site, :left)]
-    T = env.bra[site]
-    M = env.mpo[site]
-    B = env.ket[site]
-    RE = env.env[(site, :right)]
-    LL = update_env_left(LE, T, M, B)
-    @tensor scalar = LL[t, c, b] * RE[b, c, t]
-    scalar
+    @tensor LL[t, c, b] * RE[b, c, t]
 end
-
-
-#=
-is_left_normalized(ψ::Mps) = all(
-    I(size(A, 3)) ≈ @tensor Id[x, y] := conj(A[α, σ, x]) * A[α, σ, y] order = (α, σ) for A ∈ values(ψ.ket)
-)
-
-is_right_normalized(ϕ::Mps) = all(
-    I(size(B, 1)) ≈ @tensor Id[x, y] := B[x, σ, α] * conj(B[y, σ, α]) order = (α, σ) for B in values(ϕ.ket)
-)
-=#
-
-
-
 
 

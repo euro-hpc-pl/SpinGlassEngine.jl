ae142fa162cbddea8e67f932826512c204722a48
bartekGardas <bartek.gardas@gmail.com>
2021-09-23 22:55:34 +0200

    add Mps and Mpo to Enviroment

--- src/compressions.jl
+++ src/compressions.jl
@@ -1,34 +1,47 @@
 export 
-    Mps, Mpo
+    Mps, 
+    Mpo
+
 
 abstract type AbstractEnvironment end
+abstract type AbstractMps end
+abstract type AbstractMpo end
 
 
-mutable struct Mps
-    ket::Dict
+const AbstractTN = Union{AbstractMps, AbstractMpo}
+const Site = Union{Int, Rational{Int}}
+
+
+mutable struct Mps <: AbstractMps
+    tensors
     sites
     Mps(ket::Dict) = new(ket, sort(collect(keys(ket))))
 end
 
 
-mutable struct Mpo 
-    op::Dict
+@inline Base.getindex(ket::AbstractTN, i) = getindex(ket.tensors, i)
+@inline Base.setindex!(ket::AbstractTN, A::AbstractArray, i::Int) = ket.tensors[i] = A
+@inline Base.length(ket::AbstractTN) = length(ket.tensors)
+
+
+mutable struct Mpo <: AbstractMpo
+    tensors
     sites
     Mpo(op::Dict) = new(op, sort(collect(keys(op))))
 end
 
 
 mutable struct Environment <: AbstractEnvironment
-    bra::Dict  # to be optimized
-    mpo::Dict
-    ket::Dict
-    transpose::Bool
+    bra::Mps  # to be optimized
+    mpo::Mpo
+    ket::Mps
+    trans::Symbol
     env::Dict
 
     function Environment(
-        bra::Dict,
-        mpo::Dict,
-        ket::Dict,
+        bra::Mps,
+        mpo::Mpo,
+        ket::Mps,
         trans::Symbol=:c
     )
         @assert trans ∈ (:n, :c)
@@ -50,9 +63,9 @@ end
 
 
 function SpinGlassTensors.compress!(
-    bra::Dict,
-    mpo::Dict,
-    ket::Dict,
+    bra::Mps,
+    mpo::Mpo,
+    ket::Mps,
     Dcut::Int,
     tol::Number=1E-8,
     max_sweeps::Int=4,
@@ -60,13 +73,13 @@ function SpinGlassTensors.compress!(
 )
     env = Environment(bra, mpo, ket)
     overlap = Inf
-    overlap_before = measure_env(env, last(env.sites_bra))
+    overlap_before = measure_env(env, last(env.bra.sites))
 
     for sweep ∈ 1:max_sweeps
         _left_sweep_var!(env, args...)
         _right_sweep_var!(env, args...)
 
-        overlap = measure_env(env, last(env.sites_bra))
+        overlap = measure_env(env, last(env.bra.sites))
 
         Δ = abs(overlap_before - abs(overlap))
         @info "Convergence" Δ
@@ -108,7 +121,7 @@ function _right_sweep_var!(env::Environment, args...)
 end
 
 
-function _left_nbrs_site(site, sites)
+function _left_nbrs_site(site::Site, sites)
     # largest x in sites: x < site
     ls = filter(i -> i < site, sites)
     if isempty(ls) return -Inf end
@@ -116,99 +129,99 @@ function _left_nbrs_site(site, sites)
 end
 
 
-function _right_nbrs_site(site, sites)
+function _right_nbrs_site(site::Site, sites)
     # smallest x in sites: x > site
     ms = filter(i -> i > site, sites)
-    if isempty(ms) return Inf
+    if isempty(ms) return Inf end
     minimum(ms)
 end
 
 
-function update_env_left!(env::Environment, site)
+function update_env_left!(env::Environment, site::Site)
     if site <= first(env.bra.sites) return end
 
     lsite = _left_nbrs_site(site, env.bra.sites)
-    Lnew = update_env_left(
-                env.env[(lsite, :left)],
-                env.bra[lsite],
-                env.mpo[lsite],
-                env.ket[lsite],
+    LL = update_env_left(
+            env.env[(lsite, :left)],
+            env.bra[lsite],
+            env.mpo[lsite],
+            env.ket[lsite]
     )
 
     isite = _right_nbrs_site(lsite, env.mpo.sites)
 
     while isite < site
-        iM = env.mpo[isite]
-        Lnew = update_env_left(Lnew, iM)
+        M = env.mpo[isite]
+        LL = update_env_left(LL, M)
         isite = _right_nbrs_site(isite, env.mpo.sites)
     end
-    push!(env.env, (site, :left) => Lnew)
+    push!(env.env, (site, :left) => LL)
 end
 
 
-function update_env_right!(env::Environment, site)
-    if site >= last(env.sites_bra) return end
+function update_env_right!(env::Environment, site::Site)
+    if site >= last(env.bra.sites) return end
 
     rsite = _right_nbrs_site(site, env.bra.sites)
-    Rnew = update_env_right(  
-          RE = env.env[(rsite, :right)]
-          T = env.bra[rsite]
-          M = env.mpo[rsite]
-          B = env.ket[rsite]
+    RR = update_env_right(  
+            env.env[(rsite, :right)],
+            env.bra[rsite],
+            env.mpo[rsite],
+            env.ket[rsite]
     )
 
     isite = _left_nbrs_site(rsite, env.mpo.sites)
     while isite > site
-        iM = env.mpo[isite]
-        Rnew = update_env_right(Rnew, iM)
+        M = env.mpo[isite]
+        RR = update_env_right(RR, M)
         isite = _left_nbrs_site(isite, env.mpo.sites)
     end
-    push!(env.env, (site, :right) => Rnew)
+    push!(env.env, (site, :right) => RR)
 end
 
 
-function clear_env_site!(env::Environment, site)
+function clear_env_site!(env::Environment, site::Site)
     # delete!(env.env, (site, :right))
     # delete!(env.env, (site, :left))
 end
 
 
-function update_env_left(LE::S, T::S, M::S, B::S) where S <: AbstractMatrix 
-    @tensor L[nb, nc, nt] := LE[ob, oc, ot] * T[ot, α, nt] * 
+function update_env_left(LE::S, A::S, M::T, B::S) where {S, T <: AbstractArray} 
+    @tensor L[nb, nc, nt] := LE[ob, oc, ot] * A[ot, α, nt] * 
                              M[oc, α, nc, β] * B[ob, β, nb] order = (ot, α, oc, β, ob)  
     # for real there is no conjugate, otherwise conj(T)
     L
 end
 
 
-function _update_tensor_forward!(T::S, M::Dict, sites) where S <: AbstractMatrix 
+function _update_tensor_forward!(A::T, M::Dict, sites::Site) where T <: AbstractArray 
     for i ∈ sites
         if i == 0 break end
-        A = M[i]
-        @tensor T[l, x, r] := T[l, y, r] * A[y, x]
+        B = M[i]
+        @tensor A[l, x, r] := A[l, y, r] * B[y, x]
     end
 end
 
 
-function _update_tensor_backwards!(T::S, M::Dict, sites) where S <: AbstractMatrix 
+function _update_tensor_backwards!(A::T, M::Dict, sites::Site) where T <: AbstractArray 
     for i ∈ reverse(sites)
         if i == 0 break end
-        A = M[i]
-        @tensor T[l, x, r] := T[l, y, r] * A[x, y]
+        B = M[i]
+        @tensor A[l, x, r] := A[l, y, r] * B[x, y]
     end
 end
 
 
-function update_env_left(LE::S, T₀::S, M::Dict, B₀::S) where S <: AbstractMatrix 
+function update_env_left(LE::T, A₀::S, M::Dict, B₀::S) where {T, S <: AbstractArray} 
     sites = collect(sort(keys(M)))
-    _update_tensor_forward!(T₀, M, sites)
+    _update_tensor_forward!(A₀, M, sites)
     _update_tensor_backwards!(B₀, M, sites)
-    update_env_left(LE, T₀, M[0], B₀)
+    update_env_left(LE, A₀, M[0], B₀)
 end
 
 
 #=
-function update_env_left(LE, T, M, B, :transposed)  # same tensory bez wymiernych indeksow;  multiple dispatch for M sparse
+function update_env_left(LE, T, M, B, :transposed) 
     @tensor L[nb, nc, nt] := LE[ob, oc, ot] * T[ot, α, nt] * 
                              M[oc, β, nc, α] * B[ob, β, nb] order = (ot, α, oc, β, ob)  
     # for real there is no conjugate, otherwise conj(T)
@@ -216,57 +229,55 @@ function update_env_left(LE, T, M, B, :transposed)  # same tensory bez wymiernyc
 end
 =#
 
-function update_env_left(LE::S, iM::S) where S <: AbstractMatrix 
-    L
-end
 
-
-function update_env_right(RE::S, T::S, M::S, B::S)  where S <: AbstractMatrix #
-    @tensor R[nt, nc, nb] := RE[ot, oc, ob] * T[nt, α, ot] * M[nc, α, oc, β] * B[nb, β, ob] order = (ot, α, oc, β, ob)
+function update_env_right(RE::S, A::S, M::T, B::S)  where {T, S <: AbstractArray}
+    @tensor R[nt, nc, nb] := RE[ot, oc, ob] * A[nt, α, ot] *
+                             M[nc, α, oc, β] * B[nb, β, ob] order = (ot, α, oc, β, ob)
     # for real there is no conjugate, otherwise conj(T)
     R
 end
 
 
-function update_env_right(RE::S, T₀::S, M::Dict, B₀::S) where S <: AbstractMatrix 
+function update_env_right(RE::S, A₀::S, M::Dict, B₀::S) where {T, S <: AbstractArray} 
     sites = collect(sort(keys(M)))
-    _update_tensor_forward!(T₀, M, sites)
+    _update_tensor_forward!(A₀, M, sites)
     _update_tensor_backwards!(B₀, M, sites)
-    update_env_right(RE, T₀, M[0], B₀)
+    update_env_right(RE, A₀, M[0], B₀)
 end
 
 #=
-function update_env_right(RE, T, M, B, :transposed)  # same tensory bez wymiernych indeksow
-    @tensor R[nt, nc, nb] := RE[ot, oc, ob] * T[nt, α, ot] * M[nc,  β, oc, α] * B[nb, β, ob] order = (ot, α, oc, β, ob)
+function update_env_right(RE, T, M, B, :transposed)  
+    @tensor R[nt, nc, nb] := RE[ot, oc, ob] * T[nt, α, ot] * 
+                             M[nc,  β, oc, α] * B[nb, β, ob] order = (ot, α, oc, β, ob)
     # for real there is no conjugate, otherwise conj(T)
     R
 end
 =#
 
-function update_env_right(RE::S, iM::S) where S <: AbstractMatrix 
-    @tensor R[nt, nc, nb] := iM[nc, oc] * RE[nt, oc, nb]
+function update_env_right(RE::T, M::S) where {T, S <: AbstractArray} 
+    @tensor R[nt, nc, nb] := M[nc, oc] * RE[nt, oc, nb]
     R
 end
 
 
-function project_ket_on_bra(env::Environment, site)
+function project_ket_on_bra(env::Environment, site::Site)
     project_ket_on_bra(
-        env.env[(site, :left)]
-        env.ket[site]
-        env.mpo[site]
+        env.env[(site, :left)],
+        env.ket[site],
+        env.mpo[site],
         env.env[(site, :right)]
     )
 end
 
 
-function project_ket_on_bra(LE::S, B::S, M::S, RE::S) where S <: AbstractMatrix
-    @tensor T[x, y, z] := LE[k, l, x] * B[k, m, o] * 
+function project_ket_on_bra(LE::S, B::S, M::T, RE::S) where {T, S <: AbstractArray}
+    @tensor C[x, y, z] := LE[k, l, x] * B[k, m, o] * 
                           M[l, y, n, m] * RE[z, n, o] order = (k, l, m, n, o)
-    T
+    C
 end
 
 
-function project_ket_on_bra(LE::S, B₀::S, M::Dict, RE::S) where S <: AbstractMatrix
+function project_ket_on_bra(LE::S, B₀::S, M::Dict, RE::S) where S <: AbstractArray
     sites = collect(sort(keys(M)))
     _update_tensor_forward!(B₀, M, sites)
     T = project_ket_on_bra(LE, B₀, M[0], RE)
@@ -282,15 +293,14 @@ function project_ket_on_bra(LE, B, M, RE, :transposed)
 end
 =#
 
-function measure_env(env::Environment, site)
-    LL = update_env_left(
-        env.env[(site, :left)]
-        env.bra[site]
-        env.mpo[site]
-        env.ket[site]
+function measure_env(env::Environment, site::Site)
+    L = update_env_left(
+            env.env[(site, :left)],
+            env.bra[site],
+            env.mpo[site],
+            env.ket[site],
     )
-    LE = env.env[(site, :left)]
-    @tensor LL[t, c, b] * RE[b, c, t]
+    R = env.env[(site, :right)]
+    @tensor L[t, c, b] * R[b, c, t]
 end
-
 
--- test/compressions.jl
+++ test/compressions.jl
@@ -1,4 +1,4 @@
-function SpinGlassTensors.MPS(ket::Dict)
+function SpinGlassTensors.MPS(ket::Mps)
     L = length(ket)
     ϕ = MPS(eltype(ket[1]), L) 
     for i ∈ 1:L ϕ[i] = ket[i] end
@@ -9,7 +9,7 @@ Base.Dict(
     ϕ::SpinGlassTensors.AbstractTensorNetwork
 ) = Dict(i => A for (i, A) ∈ enumerate(ϕ))
 
-#=
+
 @testset "Compressions for sparse mps and mpo works" begin
     D = 16
     d = 2
@@ -23,8 +23,8 @@ Base.Dict(
     ψ = randn(MPS{T}, sites, D, d)
     W = randn(MPO{T}, sites, D, d)
 
-    ket = Dict(ψ)
-    mpo = Dict(W)
+    ket = Mps(Dict(ψ))
+    mpo = Mpo(Dict(W))
 
     @testset "Two mps representations are compressed to the same state" begin 
         χ = W * ψ
@@ -34,90 +34,15 @@ Base.Dict(
 
         #ϕ = copy(ψ)
         #canonise!(ϕ, :left)
-        #bra = Dict(ϕ)
+        #bra = Mps(Dict(ϕ)0
 
-        bra = copy(Dict(χ))
+        bra = Mps(copy(Dict(χ)))
         @time overlap = compress!(bra, mpo, ket, Dcut, tol, max_sweeps)
         println(overlap)
         
         ϕ = MPS(bra)
         @time is_right_normalized(ϕ)
         @test norm(χ) ≈ norm(ϕ) ≈ 1
-        @test dot(ϕ, χ) ≈ dot(χ, ϕ) ≈ 1 
-    end
-end
-=#
-
-@testset "Contraction" begin
-    D = 16
-    d = 2
-    sites = 100
-    T = Float64
-    
-    #Dcut = 8
-    #max_sweeps = 10
-    #tol = 1E-10
-
-    M = randn(MPS{T}, sites, D, d)
-    W = randn(MPS{T}, sites, D, d)
-    ψ = Mps(Dict(M))
-    ϕ = Mps(Dict(W))
-
-    @testset "dot products" begin
-        @testset "is equal to itself" begin
-            @test dot(ψ, ψ) ≈ dot(ψ, ψ)
-        end
-    
-        @testset "change of arguments results in conjugation" begin
-            @test dot(ψ, ϕ) ≈ conj(dot(ϕ, ψ))
-        end
-    
-        @testset "norm is 2-norm" begin
-            @test norm(ψ) ≈ sqrt(abs(dot(ψ, ψ)))
-        end
-    
-        @testset "renormalizations" begin
-            ψ.ket[ψ.sites_ket[end]] *= 1/norm(ψ)
-            @test dot(ψ, ψ) ≈ 1
-    
-            ϕ.ket[ψ.sites_ket[1]] *= 1/norm(ϕ)
-            @test dot(ϕ, ϕ) ≈ 1
-        end
-    
-    end 
-end
-
-@testset "Canonisation" begin
-
-    D = 16
-    Dcut = 8
-    
-    d = 2
-    sites = 100
-    
-    T = Float64
-    
-    M = randn(MPS{T}, sites, D, d)
-    W = randn(MPS{T}, sites, D, d)
-    ψ = Mps(Dict(M))
-    ϕ = Mps(Dict(W))
-    
-    
-    @testset "Canonisation (left)" begin
-        a = norm(ψ)
-        b = canonise(ψ, :left, Dcut)
-        @test a ≈ b
-        #println(a, b)
-        #@test is_left_normalized(ψ)
-        @test dot(ψ, ψ) ≈ 1
-    end
-    
-    @testset "Canonisation (right)" begin
-        a = norm(ϕ)
-        b = canonise(ϕ, :right, Dcut)
-        @test a ≈ b 
-        #println(a, b)
-        #@test is_right_normalized(ϕ)
-        @test dot(ϕ, ϕ) ≈ 1
+        #@test dot(ϕ, χ) ≈ dot(χ, ϕ) ≈ 1 
     end
 end

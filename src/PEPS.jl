75558df3cb56da24799aecfd53d599e8f32c7424
Łukasz Pawela <lukasz.pawela@gmail.com>
2021-10-04 21:49:28 +0200

    Merge PEPSNetwork and FusedNetwork into one type

--- src/PEPS.jl
+++ src/PEPS.jl
@@ -4,13 +4,9 @@ export
     node_from_index,
     conditional_probability
 
+const IntOrRational = Union{Int, Rational{Int}}
 
-function peps_lattice(m::Int, n::Int)
-    labels = [(i, j) for j ∈ 1:n for i ∈ 1:m]
-    LabelledGraph(labels, grid((m, n)))
-end
-
-struct PEPSNetwork <: AbstractGibbsNetwork{NTuple{2, Int}, NTuple{2, Int}}
+struct PEPSNetwork{F} <: AbstractGibbsNetwork{NTuple{2, Int}, NTuple{2, Int}}
     factor_graph::LabelledGraph{T, NTuple{2, Int}} where T
     network_graph::LabelledGraph{S, NTuple{2, Int}} where S
     vertex_map::Function
@@ -22,14 +18,14 @@ struct PEPSNetwork <: AbstractGibbsNetwork{NTuple{2, Int}, NTuple{2, Int}}
     bond_dim::Int
     var_tol::Real
     sweeps::Int
-    gauges
-    tensor_spiecies
-    columns_MPO::NTuple{N, Union{Rational{Int}, Int}} where N
-    layers_MPS::NTuple{M, Union{Rational{Int}, Int}} where M
-    layers_left_env::NTuple{K, Union{Rational{Int}, Int}} where K
-    layers_right_env::NTuple{L, Union{Rational{Int}, Int}} where L
+    gauges::Dict{Tuple{Rational, Int}, Vector{Float64}}
+    tensor_spiecies::Dict{NTuple{2, IntOrRational}, Symbol}
+    columns_MPO::NTuple{N, IntOrRational} where N
+    layers_MPS::NTuple{M, IntOrRational} where M
+    layers_left_env::NTuple{K, IntOrRational} where K
+    layers_right_env::NTuple{L, IntOrRational} where L
 
-    function PEPSNetwork(
+    function PEPSNetwork{F}(
         m::Int,
         n::Int,
         factor_graph::LabelledGraph,
@@ -39,17 +35,12 @@ struct PEPSNetwork <: AbstractGibbsNetwork{NTuple{2, Int}, NTuple{2, Int}}
         var_tol::Real=1E-8,
         sweeps::Int=4,
         columns_MPO = (-1//2, 0),  # from left to right
-        # with gauges
-        # layers_MPS=(1//6, 0, -3//6, -4//6),  # from bottom to top
-        # layers_left_env=(1//6,),
-        # layers_right_env=(0, -3//6)
-        # with gauges but differently
         layers_MPS=(4//6, 3//6, 0, -1//6),  # from bottom to top
         layers_left_env=(4//6, 3//6),
         layers_right_env=(0, -3//6)
     )
         vmap = vertex_map(transformation, m, n)
-        ng = peps_lattice(m, n)
+        ng = F ? cross_lattice(m, n) : peps_lattice(m, n)
         nrows, ncols = transformation.flips_dimensions ? (n, m) : (m, n)
 
         if !is_compatible(factor_graph, ng)
@@ -57,39 +48,63 @@ struct PEPSNetwork <: AbstractGibbsNetwork{NTuple{2, Int}, NTuple{2, Int}}
         end
 
         _types = (:site, :central_h, :central_v, :gauge_h)
-        _gauges = Dict()
-        _tensor_spiecies = Dict()
+        _gauges = Dict{Tuple{Rational, Int}, Vector{Float64}}()
+        _tensor_spiecies = Dict{NTuple{2, IntOrRational}, Symbol}()
 
-        network = new(factor_graph, ng, vmap, m, n, nrows, ncols, β, bond_dim,
-                      var_tol, sweeps, _gauges, _tensor_spiecies,
-                      columns_MPO, layers_MPS, layers_left_env, layers_right_env
-                )
-        for type ∈ _types
-            push!(network.tensor_spiecies, tensor_assignment(network, type)...)
-        end
-
+        network = new(
+            factor_graph,
+            ng,
+            vmap,
+            m,
+            n,
+            nrows,
+            ncols,
+            β,
+            bond_dim,
+            var_tol,
+            sweeps,
+            _gauges,
+            _tensor_spiecies,
+            columns_MPO,
+            layers_MPS,
+            layers_left_env,
+            layers_right_env
+        )
+        
+        assign_tensors!(network)
         update_gauges!(network, :id)
         network
     end
 end
 
-
-function projectors(network::PEPSNetwork, vertex::NTuple{2, Int})
+function projectors(network::PEPSNetwork{false}, vertex::NTuple{2, Int})
     i, j = vertex
     neighbours = ((i, j-1), (i-1, j), (i, j+1), (i+1, j))
     projector.(Ref(network), Ref(vertex), neighbours)
 end
 
+function projectors(network::PEPSNetwork{true}, vertex::NTuple{2, Int})
+    i, j = vertex
+    neighbours = (
+                    (
+                        (i+1, j-1), (i, j-1), (i-1, j-1)
+                    ),
+                    (i-1, j),
+                    (
+                        (i+1, j+1), (i, j+1), (i-1, j+1)
+                    ),
+                    (i+1, j)
+                )
+    projector.(Ref(network), Ref(vertex), neighbours)
+end
 
 node_index(peps::AbstractGibbsNetwork, node::NTuple{2, Int}) = peps.ncols * (node[1] - 1) + node[2]
-_mod_wo_zero(k, m) = k % m == 0 ? m : k % m
 
 
 node_from_index(peps::AbstractGibbsNetwork, index::Int) =
-    ((index-1) ÷ peps.ncols + 1, _mod_wo_zero(index, peps.ncols))
+    ((index-1) ÷ peps.ncols + 1, mod1(index, peps.ncols))
 
-
-function boundary(peps::PEPSNetwork, node::NTuple{2, Int})
+function boundary(peps::PEPSNetwork{false}, node::NTuple{2, Int})
     i, j = node
     x = (-4, -2)
     vcat(
@@ -103,8 +118,31 @@ function boundary(peps::PEPSNetwork, node::NTuple{2, Int})
     )
 end
 
+function boundary(peps::FusedNetwork{true}, node::NTuple{2, Int})
+    i, j = node
+    vcat(
+        [
+            [((i, k-1), (i+1, k), (i, k), (i+1, k-1)), ((i, k), (i+1, k))] for k ∈ 1:j-1
+        ]...,
+        (
+            (i, j-1), (i+1, j)
+        ),
+        (
+            (i, j-1), (i, j)
+        ),
+        (
+            (i-1, j-1), (i, j)
+        ),
+        (
+            (i-1, j), (i, j)
+        ),
+        [
+            [((i-1, k-1), (i, k), (i-1, k), (i, k-1)), ((i-1, k), (i, k))] for k ∈ j+1:peps.ncols
+        ]...
+    )
+end
 
-function conditional_probability(peps::PEPSNetwork, w::Vector{Int})
+function conditional_probability(peps::PEPSNetwork{false}, w::Vector{Int})
     i, j = node_from_index(peps, length(w)+1)
     ∂v = boundary_state(peps, w, (i, j))
 
@@ -121,6 +159,22 @@ function conditional_probability(peps::PEPSNetwork, w::Vector{Int})
     normalize_probability(prob)
 end
 
+function conditional_probability(peps::PEPSNetwork{true}, w::Vector{Int})
+    i, j = node_from_index(peps, length(w)+1)
+    ∂v = boundary_state(peps, w, (i, j))
+
+    L = left_env(peps, i, ∂v[1:2*j-2])
+    R = right_env(peps, i, ∂v[2*j+3 : 2*peps.ncols+2])
+    A = reduced_site_tensor(peps, (i, j), ∂v[2*j-1], ∂v[2*j], ∂v[2*j+1], ∂v[2*j+2])
+
+    ψ = dressed_mps(peps, i)
+    MX, M = ψ[2*j-1], ψ[2*j]
+
+    @tensor prob[σ] := L[x] * MX[x, m, y] * M[y, l, z] * R[z, k] *
+                        A[k, l, m, σ] order = (x, y, z, k, l, m)
+
+    normalize_probability(prob)
+end
 
 function bond_energy(
     network::AbstractGibbsNetwork,
@@ -141,10 +195,18 @@ function bond_energy(
     vec(energies)
 end
 
-
-function update_energy(network::PEPSNetwork, σ::Vector{Int})
+function update_energy(network::PEPSNetwork{false}, σ::Vector{Int})
     i, j = node_from_index(network, length(σ)+1)
     bond_energy(network, (i, j), (i, j-1), local_state_for_node(network, σ, (i, j-1))) +
     bond_energy(network, (i, j), (i-1, j), local_state_for_node(network, σ, (i-1, j))) +
+    local_energy(network, (i, j))
+end
+
+function update_energy(network::PEPSNetwork{true}, σ::Vector{Int})
+    i, j = node_from_index(network, length(σ)+1)
+    bond_energy(network, (i, j), (i, j-1), local_state_for_node(network, σ, (i, j-1))) +
+    bond_energy(network, (i, j), (i-1, j), local_state_for_node(network, σ, (i-1, j))) +
+    bond_energy(network, (i, j), (i-1, j-1), local_state_for_node(network, σ, (i-1, j-1))) +
+    bond_energy(network, (i, j), (i-1, j+1), local_state_for_node(network, σ, (i-1, j+1))) +
     local_energy(network, (i, j))
 end

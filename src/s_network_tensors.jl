b38f3c490b87207858de4b4bd85fc7cce3d2e58a
Marek Rams <marek.rams@uj.edu.pl>
2021-10-05 16:38:22 +0200

    cleaning compress

--- src/s_network_tensors.jl
+++ src/s_network_tensors.jl
@@ -383,9 +383,9 @@ IdentityMps(peps::AbstractGibbsNetwork) = Mps(   ## change for pegazus
     ψ = mps(peps, i+1)
     W = mpo(peps, peps.mpo_main, i)
     ψ0 = dot(W, ψ)
-    for (jj, TTT) in ψ0.tensors
-        println("site = ", jj, " tensor = ", size(TTT))
-    end
+    # for (jj, TTT) in ψ0.tensors
+    #     println("site = ", jj, " tensor = ", size(TTT))
+    # end
     # when to compress?
     compress!(ψ0, W, ψ, peps.bond_dim, peps.var_tol, peps.sweeps) 
     ψ0

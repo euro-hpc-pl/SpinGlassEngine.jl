ada9eeb03b581021944f7cb72a7c5e44eb205451
tomsmierz <smierzchalski.tomek@gmail.com>
2021-10-29 12:13:56 +0100

    GaugesEnergy

--- test/runtests.jl
+++ test/runtests.jl
@@ -54,7 +54,7 @@ push!(my_tests,
         "compressions.jl" =#
         #"search_full_chimera.jl",
         "search_chimera.jl",
-        "search_cross.jl",
+        #"search_cross.jl",
         #"canonise.jl"
 )
 

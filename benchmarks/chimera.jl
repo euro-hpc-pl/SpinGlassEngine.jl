52d9f139d46d52749b950466616117a368876cd2
tomsmierz <smierzchalski.tomek@gmail.com>
2021-11-29 12:01:53 +0100

    benchmark script

---
+++ benchmarks/chimera.jl
@@ -0,0 +1,66 @@
+using SpinGlassEngine
+using Logging
+using SpinGlassNetworks, SpinGlassTensors
+using LightGraphs
+using LinearAlgebra
+using TensorCast
+using MetaGraphs
+using CSV
+
+function bench()
+    m = 16
+    n = 16
+    t = 8
+    L = n * m * t
+
+    bond_dim = 128
+    δp = 1E-2
+    num_states = 100
+    variational_tol = 1E-8
+    max_num_sweeps = 4
+    
+    open("/home/tsmierzchalski/tensor/chimera.csv", "w") do file
+
+        for i ∈ 1:100
+            if i < 10
+                instance = "/home/tsmierzchalski/tensor/chimera2048_spinglass_power/00$(i).txt"
+
+            elseif i ∈ 10:99
+                instance = "/home/tsmierzchalski/tensor/chimera2048_spinglass_power/0$(i).txt"
+            
+            else 
+                instance = "/home/tsmierzchalski/tensor/chimera2048_spinglass_power/$(i).txt"
+            end
+            for β ∈ [1., 2., 3., 4., 5., 6., 7., 8.]
+
+            
+
+                ig = ising_graph(instance)
+
+                fg = factor_graph(
+                    ig,
+                    #spectrum=full_spectrum,
+                    cluster_assignment_rule=super_square_lattice((m, n, t))
+                )
+
+                params = MpsParameters(bond_dim, variational_tol, max_num_sweeps)
+                search_params = SearchParameters(num_states, δp)
+
+                for Strategy ∈ (SVDTruncate, ), Sparsity ∈ (Dense, )
+                    for Layout ∈ (EnergyGauges, GaugesEnergy, EngGaugesEng), transform ∈ rotation.([0])
+                        #write(file, i, β, Strategy, Sparsity, Layout, transform)
+
+                        network = PEPSNetwork{Square{Layout}, Sparsity}(m, n, fg, transform)
+                        ctr = MpsContractor{Strategy}(network, [β], params)
+
+                        sol = low_energy_spectrum(ctr, search_params, merge_branches(network))
+                        data = Dict("i" => i, "β" =>β, "Strategy" => Strategy, "Sparsity" => Sparsity, "Layout" => Layout, "transform" => transform, "energies" => sol.energies[1:1])
+                        CSV.write(file, data)
+                    end
+                end
+            end
+        end
+    end
+end
+
+bench()

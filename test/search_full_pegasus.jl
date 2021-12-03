d2fe8d94c8e2881071763931f324256017982454
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2021-12-03 11:14:10 +0100

    fix bug

--- test/search_full_pegasus.jl
+++ test/search_full_pegasus.jl
@@ -30,7 +30,7 @@ function bench(instance::String)
         for Layout ∈ (EnergyGauges, ), transform ∈ rotation.([0])
             println((Strategy, Sparsity, Layout, transform))
 
-            @time network = PEPSNetwork{Square{Layout}, Sparsity}(m, n, fg, transform)
+            @time network = PEPSNetwork{SquareStar{Layout}, Sparsity}(m, n, fg, transform)
             @time ctr = MpsContractor{Strategy}(network, [β/8, β/4, β/2, β], params)
 
             @time sol = low_energy_spectrum(ctr, search_params, merge_branches(network))

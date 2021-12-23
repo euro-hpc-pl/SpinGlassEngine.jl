a1388d78006d720ec5c1a014ecc50e80da7861ab
bartekGardas <bartek.gardas@gmail.com>
2021-12-23 23:53:55 +0100

    add test for energies

--- test/search_cross.jl
+++ test/search_cross.jl
@@ -1,23 +1,4 @@
 
-function _energy(ig::IsingGraph, fg, fg_state::Vector{Int})
-    ig_state = decode_factor_graph_state(fg, fg_state)
-    en = 0.0
-    for (i, σ) ∈ ig_state
-        en += get_prop(ig, i, :h) * σ
-        for (j, η) ∈ ig_state
-            if has_edge(ig, i, j)
-                J = get_prop(ig, i, j, :J)
-            elseif has_edge(ig, j, i)
-                J = get_prop(ig, j, i, :J)
-            else
-                J = 0.0
-            end
-            en += σ * J * η / 2.0
-        end
-    end
-    en
-end
-
 @testset "Pegasus-like instance has the correct ground state energy" begin
 
     ground_energy = -23.301855
@@ -34,7 +15,6 @@ end
     instance = "$(@__DIR__)/instances/pathological/cross_$(m)_$(n)_dd.txt"
 
     ig = ising_graph(instance)
-    J = couplings(ig) + Diagonal(biases(ig))
 
     fg = factor_graph(
         ig,
@@ -53,8 +33,7 @@ end
                 contractor = MpsContractor{Strategy}(network, [β/2, β], params)
                 sol = low_energy_spectrum(contractor, search_params)
                 @test sol.energies[begin] ≈ ground_energy
-
-                @test sol.energies ≈ _energy.(Ref(ig), Ref(fg), sol.states)
+                @test sol.energies ≈ energy.(Ref(ig), Ref(fg), sol.states)
                 clear_memoize_cache()
             end
         end

a8b05fae626f98ff213f05a419229b2888b7eac0
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2022-01-13 13:11:21 +0100

    add smallest cross

--- test/search_smallest_cross.jl
+++ test/search_smallest_cross.jl
@@ -1,17 +1,17 @@
 
 @testset "Pegasus-like instance has the correct ground state energy" begin
-    ground_energy = -23.301855
+    #ground_energy = -23.301855
 
-    m = 3
-    n = 4
-    t = 3
+    m = 2
+    n = 3
+    t = 1
     L = n * m * t
 
-    β = 2.
+    β = 1.
     bond_dim = 16
     num_states = 22
 
-    instance = "$(@__DIR__)/instances/pathological/cross_$(m)_$(n)_dd.txt"
+    instance = "$(@__DIR__)/instances/pathological/cross_3_2.txt"
 
     ig = ising_graph(instance)
 
@@ -23,22 +23,22 @@
     params = MpsParameters(bond_dim, 1E-8, 4)
     search_params = SearchParameters(num_states, 0.0)
 
-    for Strategy ∈ (SVDTruncate, MPSAnnealing), Sparsity ∈ (Sparse, Dense)
+    for Strategy ∈ (SVDTruncate, MPSAnnealing), Sparsity ∈ (Dense,)
         for Layout ∈ (EnergyGauges, GaugesEnergy, EngGaugesEng)
             for transform ∈ all_lattice_transformations
                 net = PEPSNetwork{SquareStar{Layout}, Sparsity}(m, n, fg, transform)
-                ctr = MpsContractor{Strategy}(net, [β/2, β], params)
+                ctr = MpsContractor{Strategy}(net, [β/8., β/4., β/2., β], params)
                 sol = low_energy_spectrum(ctr, search_params)
 
                 ig_states = decode_factor_graph_state.(Ref(fg), sol.states)
-                @test sol.energies[begin] ≈ ground_energy
+                #@test sol.energies[begin] ≈ ground_energy
                 @test sol.energies ≈ energy.(Ref(ig), ig_states)
 
-                #=norm_prob = exp.(sol.probabilities)
-                exact_norm_prob = exp.(-β .* (sol.energies .- sol.energies[1]))
-                exact_norm_prob = exact_norm_prob./sum(exact_norm_prob)
-                @test norm_prob ≈ exact_norm_prob 
-=#
+                #norm_prob = exp.(sol.probabilities)
+                #exact_norm_prob = exp.(-β .* (sol.energies .- sol.energies[1]))
+                #exact_norm_prob = exact_norm_prob./sum(exact_norm_prob)
+                #@test norm_prob ≈ exact_norm_prob 
+
                 clear_memoize_cache()
             end
         end

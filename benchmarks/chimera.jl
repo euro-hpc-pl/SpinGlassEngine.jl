113abc02ce83459dc37f3a586ffd44b85851bb29
tomsmierz <smierzchalski.tomek@gmail.com>
2022-03-15 14:28:42 +0100

    benchmarking script update

--- benchmarks/chimera.jl
+++ benchmarks/chimera.jl
@@ -1,3 +1,4 @@
+using MKL
 using SpinGlassEngine
 using SpinGlassNetworks
 using SpinGlassTensors
@@ -6,48 +7,51 @@ using CSV
 using DataFrames
 
 
+
+
 disable_logging(LogLevel(1))
 
 
 function bench(instance_dir::String, out_path::String)
-    m = 16
-    n = 16
+    m = 8
+    n = 8
     t = 8
+    
     L = n * m * t
+    max_cl_states = 2^(t-0)
 
-    δp = 1E-2
-    bond_dim = 16
-    num_states = 100
-    max_num_sweeps = 4
-    variational_tol = 1E-8
-    betas = collect(1:8)
+
+    bond_dim = 32
+    dE = 5
+    
+    num_states = 1000000
+    betas = collect(1:14)
 
     dir = cd(instance_dir)
     count = 0
     for (i, instance) ∈ enumerate(readdir(join=true))
-        ig = ising_graph(instance)
 
         fg = factor_graph(
-            ig,
-            spectrum=brute_force,
-            cluster_assignment_rule=super_square_lattice((m, n, t))
-        )
+        ising_graph(instance),
+        max_cl_states,
+        spectrum=brute_force,
+        cluster_assignment_rule=super_square_lattice((m, n, t)))
 
-        params = MpsParameters(bond_dim, variational_tol, max_num_sweeps)
-        search_params = SearchParameters(num_states, δp)
+        params = MpsParameters(bond_dim, 1E-8, 10)
+        
+        for β ∈ betas, Strategy ∈ (SVDTruncate, ), Sparsity ∈ (Dense, )
+            for Layout ∈ (EnergyGauges, GaugesEnergy, EngGaugesEng), transform ∈ all_lattice_transformations
 
-        for β ∈ betas, Strategy ∈ (SVDTruncate, ), Sparsity ∈ (Dense, Sparse)
-            for Layout ∈ (EnergyGauges, GaugesEnergy, EngGaugesEng), transform ∈ rotation.([0])
-                  
-                  
+                δp = 1E-5*exp(-β * dE)  
+                search_params = SearchParameters(num_states, δp)
+
                 data = try
 
                     net = PEPSNetwork{Square{Layout}, Sparsity}(m, n, fg, transform)
-                    ctr = MpsContractor{Strategy}(net, [β], params)
+                    ctr = MpsContractor{Strategy}(net, [β/6, β/3, β/2, β], params)
                     times = @elapsed sol = low_energy_spectrum(ctr, search_params, merge_branches(ctr))
 
-                    data = DataFrame(:i => i, :β => β, :Strategy => Strategy,
-                    :Sparsity => Sparsity, :Layout => Layout,
+                    data = DataFrame(:i => i, :β => β, :Layout => Layout,
                     :transform => transform, :energies => sol.energies[1:1],
                     :time => times)
 
@@ -56,8 +60,7 @@ function bench(instance_dir::String, out_path::String)
                 catch e 
                     
 
-                    data = DataFrame(:i => i, :β => β, :Strategy => Strategy,
-                    :Sparsity => Sparsity, :Layout => Layout,
+                    data = DataFrame(:i => i, :β => β, :Layout => Layout,
                     :transform => transform, :energies => "ERROR",
                     :time => "ERROR")
                         
@@ -79,6 +82,6 @@ end
 
 
 bench(
-    "/home/tsmierzchalski/tensor/chimera2048_spinglass_power",
-    "/home/tsmierzchalski/tensor/chimera2048_97.csv"
+    "/home/tsmierzchalski/tensor/new/source/chimera512_spinglass_power",
+    "/home/tsmierzchalski/tensor/new/results/chimera512.csv"
     )

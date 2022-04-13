32cd2e4bb92d6e2e0bffafbd7511bfe4568241e5
Bartek Gardas <bartek.gardas@gmail.com>
2022-04-13 23:50:14 +0200

    add py script for the f#$k sake

---
+++ benchmarks/branch_states.py
@@ -0,0 +1,25 @@
+import numpy as np
+import timeit
+
+def branch_states(states, num_states):
+    (k, l) = states.shape
+    ns = np.empty((k, num_states, l+1), dtype=int)
+    ns[:, :, :l] = states.reshape(k, 1, l)
+    p = np.arange(num_states, dtype=int)
+    ns[:, :, l] = p.reshape(1, 1, num_states)
+    ns.reshape(k * num_states, l+1)
+    return ns
+
+
+if __name__ == '__main__':
+
+    nstates = 1000
+    lstate = 128
+    num_states = 256
+    
+    st = np.ones((nstates, lstate), dtype=int)
+
+    tic = timeit.timeit()
+    branch_states(st, num_states)
+    toc = timeit.timeit()
+    print(tic - toc)

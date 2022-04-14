1ed0f9f3b33cff217920d5886a7574cb2109e88b
Bartek Gardas <bartek.gardas@gmail.com>
2022-04-14 12:20:55 +0200

    from into to float

--- benchmarks/branch_states.jl
+++ benchmarks/branch_states.jl
@@ -1,38 +1,42 @@
 
 
 # This is what we have in search.jl and it is SLOW
-@inline branch_state(local_basis::Vector{Int}, σ::Vector{Int}) = vcat.(Ref(σ), local_basis)
-function branch_states_v1(num_states::Int, states::Vector{Vector{Int}})
+@inline function branch_state(local_basis::Vector{Int}, σ::Vector{T}) where T <: Number
+    vcat.(Ref(σ), local_basis)
+end
+function branch_states_v1(num_states::Int, states::Vector{Vector{T}}) where T <: Number
     local_basis = collect(1:num_states)
     vcat(branch_state.(Ref(local_basis), states)...)
 end
 
 # This is what we want to have in search.jl but it is also SLOW (python is FAST)
-function branch_states_v2(num_states::Int, states::Matrix{Int})
+function branch_states_v2(num_states::Int, states::Matrix{T}) where T <: Number
     lstate, nstates = size(states)
     L = num_states * nstates
     local_basis = collect(1:num_states)
-    new_states = Array{Int}(undef, lstate + 1, L)
+    new_states = Array{T}(undef, lstate + 1, L)
     new_states[1:lstate, :] = repeat(states, outer=[1, num_states])
     new_states[lstate+1, :] = repeat(local_basis, outer=[nstates])
     new_states
 end
 
 # This is the same as in branch_states_v2 but array broadcasting is wrong.
-function branch_states_v3(num_states::Int, states::Matrix{Int})
+function branch_states_v3(num_states::Int, states::Matrix{T}) where T <: Number
     lstate, nstates = size(states)
     local_basis = collect(1:num_states)
-    ns = Array{Int}(undef, lstate+1, num_states, nstates)
+    ns = Array{T}(undef, lstate+1, num_states, nstates)
     # Does the broadcasting work?
     ns[1:lstate, :, :] .= reshape(states, lstate, 1, nstates)
     ns[lstate, :, :] .= reshape(local_basis, num_states, 1, 1)
     reshape(ns, lstate+1, nstates * num_states)
 end
 
+T = Float64
+
 nstates = 1000
 lstate = 128
 
-mat_states = ones(Int, lstate, nstates);
+mat_states = ones(T, lstate, nstates);
 vec_states = [mat_states[:, i]  for i in 1:nstates];
 
 num_states = 256

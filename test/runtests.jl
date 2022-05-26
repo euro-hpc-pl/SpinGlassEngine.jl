0cd18ad87786d2307b82a43b2b9383df100858ce
marekrams <marek.rams@uj.edu.pl>
2022-05-26 13:04:08 +0200

    pegasus_nd boundary

--- test/runtests.jl
+++ test/runtests.jl
@@ -45,7 +45,7 @@ push!(my_tests,
         #"search_MPS.jl",
         #"search_chimera_pathological.jl",
         #"search_chimera_smallest.jl", # problem with MPSAnnealing
-        "search_chimera_full.jl",   #  ground is incorrect
+        #"search_chimera_full.jl",   #  ground is incorrect
         #"search_cross_square_star.jl",  # final test fails
         #"search_smallest_cross_square_star.jl",
         #"search_pegasus_square_star.jl"
@@ -53,16 +53,16 @@ push!(my_tests,
 )
 
 # This is work in progress (may or may not be included in the released version)
-#=
+
 push!(my_tests,
         #"future_tests/chimera_overlap_python.jl", # OK
         #"future_tests/cross_square_star_prob.jl",
         #"future_tests/search_new_geometry_nodiag.jl", # NO
-        #"future_tests/pegasus_nondiag_geometry.jl", # NO
+        "future_tests/pegasus_nondiag_geometry.jl", # NO
         #"future_tests/gauges.jl", # O
         #"future_tests/memoization.jl",
 )
-=#
+
 
 for my_test in my_tests
     include(my_test)

1deabcc37dd0631bfc271c1c4122f5ca3d0e5ce7
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2022-06-30 14:42:11 +0200

    Tomek's instances are compatible

--- test/runtests.jl
+++ test/runtests.jl
@@ -61,11 +61,12 @@ push!(my_tests,
     # "experimental/chimera_overlap_python.jl", # OK
     # "experimental/cross_square_star_prob.jl",
     # "experimental/pegasus_nondiag_basic.jl",
-    #"experimental/pegasus_nondiag_heavy.jl",
+    # "experimental/pegasus_nondiag_heavy.jl",
     # "experimental/gauges.jl",
     # "experimental/memoization.jl",
     # "experimental/experiments_with_sparse_chimera.jl",
-    "experimental/pegasus_masoud.jl"
+    # "experimental/pegasus_masoud.jl",
+    "experimental/pegasus_diag_basic.jl"
 )
 
 

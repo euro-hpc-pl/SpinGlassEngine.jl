01db23adc144ec084b02abc72974151ce487f1e7
tomsmierz <smierzchalski.tomek@gmail.com>
2022-07-07 14:27:46 +0200

    general PEPS network

--- src/zephyr.jl
+++ src/zephyr.jl
@@ -12,32 +12,40 @@ $(TYPEDSIGNATURES)
 function ZephyrSquare(m::Int, n::Int)
     labels = [(i, j, k) for i ∈ 1:2*n for j ∈ 1:2*m for k ∈ 1:2]  # change for bigger zephyr
     lg = LabelledGraph(labels)
-    for i ∈ 1:2*n, j ∈ 1:2*m add_edge!(lg, (i, j, 1), (i, j, 2)) end
+    for i ∈ 1:2*n, j ∈ j_function(i, n) add_edge!(lg, (i, j, 1), (i, j, 2)) end
 
     # horizontals
-    for i ∈ 1:n, j ∈ 1:m
+    for i ∈ 1:2*n, j ∈ j_function(i, n)[begin:end-1]
         add_edge!(lg, (i, j, 1), (i, j+1, 2))
         add_edge!(lg, (i, j, 2), (i, j+1, 1))
-        add_edge!(lg, (i+1, j, 2), (i+1, j+1, 1))
-        add_edge!(lg, (i+1, j, 1), (i+1, j+1, 2))
     end
 
     #vertical
-    for i ∈ 1:n, j ∈ 1:m
+    for i ∈ 1:2*n - 1 , j ∈ j_function(i, n)
         add_edge!(lg, (i, j, 1), (i+1, j, 2))
         add_edge!(lg, (i, j, 2), (i+1, j, 1))
-        add_edge!(lg, (i, j+1, 2), (i+1, j+1, 1))
-        add_edge!(lg, (i, j+1, 1), (i+1, j+1, 2))
 
     end
 
     # diagonals
-     for i ∈ 1:m, j ∈ 1:n
+     for i ∈ 1:2*n - 1, j ∈ j_function(i, n)[begin:end-1]
          add_edge!(lg, (i, j, 2), (i+1, j+1, 2))
          add_edge!(lg, (i+1, j, 1), (i, j+1, 1))
      end
     lg
 end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function j_function(i::Int, n::Int)
+    if i in collect(1:n)
+        return collect((n + 1 - i):(n + i))
+    else 
+        return collect((i-n):(3*n + 1 - i))
+    end
+end
+
 
 """
 $(TYPEDSIGNATURES)

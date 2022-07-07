dc5023254e5a2439a78e68435d2e7a1075cbb58c
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2022-07-07 12:53:57 +0200

    network is compatible with fg for zephyr Z1

--- test/runtests.jl
+++ test/runtests.jl
@@ -61,10 +61,11 @@ push!(my_tests,
     # "experimental/chimera_overlap_python.jl", # OK
     # "experimental/cross_square_star_prob.jl",
     #"experimental/pegasus_nondiag_basic.jl",
-    "experimental/pegasus_nondiag_heavy.jl",
+    #"experimental/pegasus_nondiag_heavy.jl",
     # "experimental/gauges.jl",
     # "experimental/memoization.jl",
     #"experimental/experiments_with_sparse_chimera.jl",
+    "experimental/zephyr_lattice.jl"
 )
 
 

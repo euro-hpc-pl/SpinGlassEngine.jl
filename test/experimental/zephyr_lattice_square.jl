07503c840b96c0b5b801ddbacdc008f2a5bc1a1c
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2022-07-14 17:07:51 +0200

    fix bug

--- test/experimental/zephyr_lattice_square.jl
+++ test/experimental/zephyr_lattice_square.jl
@@ -1,8 +1,8 @@
 using SpinGlassEngine
 using SpinGlassNetworks
 
-m = 2
-n = 2
+m = 1
+n = 1
 t = 4
 
 max_cl_states = 2^2
@@ -12,13 +12,13 @@ bond_dim = 8
 δp = 1e-10
 num_states = 10
 
-ig = ising_graph("$(@__DIR__)/../instances/zephyr/z2.txt")
+ig = ising_graph("$(@__DIR__)/../instances/zephyr/z1.txt")
 
 fg = factor_graph(
     ig,
     # max_cl_states,
     spectrum=full_spectrum, #_gpu, # rm _gpu to use CPU
-    cluster_assignment_rule=zephyr_lattice_z1_5tuple((m,n,t))
+    cluster_assignment_rule=zephyr_lattice_5tuple((m,n,t))
 )
 
 clear_memoize_cache()

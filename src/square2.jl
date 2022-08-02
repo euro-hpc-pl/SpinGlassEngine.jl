d0112c78f7d99961dd519cf502302731d0d2be58
marekrams <marek.rams@uj.edu.pl>
2022-08-02 17:33:26 +0200

    starting square_diag2

--- src/square2.jl
+++ src/square2.jl
@@ -1,4 +1,4 @@
-export Square2, tensor
+export Square2, tensor, site2, update_reduced_env_right
 
 """
 $(TYPEDSIGNATURES)
@@ -27,11 +27,6 @@ function Square2(m::Int, n::Int)
         add_edge!(lg, (i, j, 1), (i, j+1, 1))
     end
 
-    # diagonals
-    # for i ∈ 1:m-1, j ∈ 1:n-1
-    #     add_edge!(lg, (i, j, 2), (i+1, j+1, 1))
-    #     add_edge!(lg, (i, j, 1), (i+1, j+1, 2))
-    # end
     lg
 end
 
@@ -106,7 +101,7 @@ function MpoLayers(::Type{T}, ncols::Int) where T <: Square2{EnergyGauges}
     right = Dict{Site, Sites}(i => (-3//6, 0) for i ∈ 1:ncols)
     for i ∈ 1:ncols - 1 push!(right, i + 1//2 => (0,)) end
 
-    #MpoLayers(main, Dict(i => (3//6, 4//6) for i ∈ 1:ncols), right)
+    #MpoLayers(main, Dict(i => (3//6, 4//6) for i ∈ 1:ncols), right)  # Check if gauge works?
     MpoLayers(main, Dict(i => (3//6,) for i ∈ 1:ncols), right)
 
 end
@@ -161,20 +156,9 @@ function conditional_probability(   # TODO
         pd1 = projector(ctr.peps, (i, j, 1), ((i+1, j, 1), (i+1, j, 2)))
         pd2 = projector(ctr.peps, (i, j, 2), ((i+1, j, 1), (i+1, j, 2)))
 
-        # pr = outer_projector(
-        #         projector(ctr.peps, (i, j, 1), ((i, j+1, 1), (i, j+1, 2))),  # r
-        #         projector(ctr.peps, (i, j, 2), ((i, j+1, 1), (i, j+1, 2))))
-        # pd = outer_projector(
-        #         projector(ctr.peps, (i, j, 1), ((i+1, j, 1), (i+1, j, 2))),  # b
-        #         projector(ctr.peps, (i, j, 2), ((i+1, j, 1), (i+1, j, 2))))
-
         le = reshape(en[2], (1, :)) .+ en12[p12[:], p21[:]]
         ele = exp.(-β .* (le .- minimum(le)))
 
-        #ten3 = zeros(maximum(pr), size(ten2, 2))
-        # for s2 ∈ 1:length(en[2])
-        #     @inbounds ten3[pr[s2], :] += ten2[s2, :]
-        # end
         @cast LM3[y, z, x] := LM[(y, z), x] (z ∈ 1:maximum(pd2))
         @cast R3[y, z, x] := R[x, (y, z)] (z ∈ 1:maximum(pr2))
 
@@ -218,84 +202,6 @@ function conditional_probability(   # TODO
     normalize_probability(probs)
 end
 
-
-
-# """
-# $(TYPEDSIGNATURES)
-# """
-# function conditional_probability(
-#     ::Type{T}, ctr::MpsContractor{S}, ∂v::Vector{Int}
-# ) where {T <: Square2, S}
-#     indβ, β = length(ctr.betas), last(ctr.betas)
-#     i, j, k = ctr.current_node
-
-#     L = left_env(ctr, i, ∂v[1:j-1], indβ)
-#     R = right_env(ctr, i, ∂v[(j+4):end], indβ)
-#     M = dressed_mps(ctr, i, indβ)[j]
-
-#     @tensor LM[y, z] := L[x] * M[x, y, z]
-#     if k == 1  # here has to avarage over s2
-#         eng_loc = [local_energy(ctr.peps, (i, j, k)) for k ∈ 1:2]
-#         el = [interaction_energy(ctr.peps, (i, j, k), (i, j-1, 2)) for k ∈ 1:2]
-#         pl = [projector(ctr.peps, (i, j, k), (i, j-1, 2)) for k ∈ 1:2]
-#         eng_l = [@view el[k][pl[k][:], ∂v[j-1+k]] for k ∈ 1:2]
-
-#         eu = [interaction_energy(ctr.peps, (i, j, k), (i-1, j, 1)) for k ∈ 1:2]
-#         pu = [projector(ctr.peps, (i, j, k), (i-1, j, 1)) for k ∈ 1:2]
-#         eng_u = [@view eu[k][pu[k][:], ∂v[j+1+k]] for k ∈ 1:2]
-
-#         en = [eng_loc[k] .+ eng_l[k] .+ eng_u[k] for k ∈ 1:2]
-
-#         en21 = interaction_energy(ctr.peps, (i, j, 2), (i, j, 1))
-#         p21 = projector(ctr.peps, (i, j, 2), (i, j, 1))
-#         p12 = projector(ctr.peps, (i, j, 1), (i, j, 2))
-
-#         pr = projector(ctr.peps, (i, j, 2), ((i, j+1, 1), (i, j+1, 2)))
-#         pd = projector(ctr.peps, (i, j, 1), ((i+1, j, 1), (i+1, j, 2)))
-
-#         ten = reshape(en[2], (:, 1)) .+ en21[p21[:], :]
-#         ten_min = minimum(ten)
-#         ten2 = exp.(-β .* (ten .- ten_min))
-#         ten3 = zeros(maximum(pr), size(ten2, 2))
-
-#         # for s2 ∈ 1:length(en[2])
-#         #     @inbounds ten3[pr[s2], :] += ten2[s2, :]
-#         # end
-
-#         RT = R[:, pr] * ten2 # R * ten3
-#         bnd_exp = dropdims(sum(LM[pd[:], :] .* RT[:, p12[:]]', dims=2), dims=2)
-#         en_min = minimum(en[1])
-#         loc_exp = exp.(-β .* (en[1] .- en_min))
-#     else  # k == 2 ; here s1 is fixed
-#         eng_loc = local_energy(ctr.peps, (i, j, 2))
-
-#         el = interaction_energy(ctr.peps, (i, j, 2), (i, j-1, 2))
-#         pl = projector(ctr.peps, (i, j, 2), (i, j-1, 2))
-#         eng_l = @inbounds @view el[pl[:], ∂v[j]]
-
-#         eu = interaction_energy(ctr.peps, (i, j, 2), (i-1, j, 1))
-#         pu = projector(ctr.peps, (i, j, 2), (i-1, j, 1))
-#         eng_u = @inbounds @view eu[pu[:], ∂v[j+3]]
-
-#         e21 = interaction_energy(ctr.peps, (i, j, 2), (i, j, 1))
-#         p21 = projector(ctr.peps, (i, j, 2), (i, j, 1))
-#         eng_21 = @inbounds @view e21[p21[:], ∂v[j+2]]
-
-#         en = eng_loc .+ eng_l .+ eng_u .+ eng_21
-#         en_min = minimum(en)
-#         loc_exp = exp.(-β .* (en .- en_min))
-
-#         pr = projector(ctr.peps, (i, j, 2), ((i, j+1, 1), (i, j+1, 2)))
-#         lmx = @inbounds @view LM[∂v[j+1], :]
-
-#         @tensor lmxR[y] := lmx[x] * R[x, y]
-#         @inbounds bnd_exp = lmxR[pr[:]]
-#     end
-
-#     probs = loc_exp .* bnd_exp
-#     push!(ctr.statistics, ((i, j, k), ∂v) => error_measure(probs))
-#     normalize_probability(probs)
-# end
 
 """
 $(TYPEDSIGNATURES)
@@ -359,8 +265,8 @@ end
 $(TYPEDSIGNATURES)
 """
 function tensor(
-    net::PEPSNetwork{Square2{S}, T}, node::PEPSNode, β::Real, ::Val{:sparse_site_square2}
-) where {T <: AbstractSparsity, S <: AbstractTensorsLayout}
+    net::PEPSNetwork{T, S}, node::PEPSNode, β::Real, ::Val{:sparse_site_square2}
+) where {T <: AbstractGeometry, S}
     @nexprs 2 k->(
         v_k = (node.i, node.j, k);
         en_k = local_energy(net, v_k)
@@ -378,8 +284,8 @@ end
 $(TYPEDSIGNATURES)
 """
 function tensor(
-    net::PEPSNetwork{Square2{S}, T}, node::PEPSNode, β::Real, ::Val{:site_square2}
-) where {T <: AbstractSparsity, S <: AbstractTensorsLayout}
+    net::PEPSNetwork{T, S}, node::PEPSNode, β::Real, ::Val{:site_square2}
+) where {T <: AbstractGeometry, S}
     sp = tensor(net, node, β, Val(:sparse_site_square2))
     for (σ, lexp) ∈ enumerate(sp.loc_exp)
         @inbounds A[getindex.(sp.projs, Ref(σ))...] += lexp
@@ -394,27 +300,74 @@ function tensor(
     network::PEPSNetwork{Square2{S}, T}, node::PEPSNode, β::Real, ::Val{:central_h}
 ) where {T <: AbstractSparsity, S <: AbstractTensorsLayout}
     i, j = node.i, floor(Int, node.j)
+    getSparseCentralTensor(network, β, (i, j), (i, j+1))
+    # p11r = projector(network, (i, j+1, 1), (i, j, 1))
+    # p21r = projector(network, (i, j+1, 1), (i, j, 2))
+    # p12r = projector(network, (i, j+1, 2), (i, j ,1))
+    # p22r = projector(network, (i, j+1, 2), (i, j, 2))
+    # p1r, (p11r, p21r) = fuse_projectors((p11r, p21r))
+    # p2r, (p12r, p22r) = fuse_projectors((p12r, p22r))
+    # p21l = projector(network, (i, j, 2), (i, j+1, 1))
+    # p22l = projector(network, (i, j, 2), (i, j+1, 2))
+    # p12l = projector(network, (i, j, 1), (i, j+1, 2))
+    # p11l = projector(network, (i, j, 1), (i, j+1, 1))
+    # p1l, (p11l, p12l) = fuse_projectors((p11l, p12l))
+    # p2l, (p21l, p22l) = fuse_projectors((p21l, p22l))
+    # e11 = interaction_energy(network, (i, j, 1), (i, j+1, 1))
+    # e12 = interaction_energy(network, (i, j, 1), (i, j+1, 2))
+    # e21 = interaction_energy(network, (i, j, 2), (i, j+1, 1))
+    # e22 = interaction_energy(network, (i, j, 2), (i, j+1, 2))
+    # e11 = e11[p11l, p11r]
+    # e21 = e21[p21l, p21r]
+    # e12 = e12[p12l, p12r]
+    # e22 = e22[p22l, p22r]
+    # le11 = exp.(-β .* (e11 .- minimum(e11)))
+    # le21 = exp.(-β .* (e21 .- minimum(e21)))
+    # le12 = exp.(-β .* (e12 .- minimum(e12)))
+    # le22 = exp.(-β .* (e22 .- minimum(e22)))
+    # sl = maximum(outer_projector(
+    #     projector(network, (i, j, 1), ((i, j+1, 1), (i, j+1, 2))),  # r
+    #     projector(network, (i, j, 2), ((i, j+1, 1), (i, j+1, 2))),  # r
+    # ))
+    # sr = maximum(outer_projector(
+    #         projector(network, (i, j+1, 1), ((i, j, 1), (i, j, 2))),  # l
+    #         projector(network, (i, j+1, 2), ((i, j, 1), (i, j, 2))),  # l
+    #     ))
+    # SparseCentralTensor(
+    #     le11,
+    #     le12,
+    #     le21,
+    #     le22,
+    #     (sl, sr)
+    # )
+end
 
-    p11r = projector(network, (i, j+1, 1), (i, j, 1))
-    p21r = projector(network, (i, j+1, 1), (i, j, 2))
-    p12r = projector(network, (i, j+1, 2), (i, j ,1))
-    p22r = projector(network, (i, j+1, 2), (i, j, 2))
 
-    p1r, (p11r, p21r) = fuse_projectors((p11r, p21r))
-    p2r, (p12r, p22r) = fuse_projectors((p12r, p22r))
+function getSparseCentralTensor(network::PEPSNetwork{T, S}, β::Real, node1::NTuple{2, Int64}, node2::NTuple{2, Int64}
+    ) where {T <: AbstractGeometry, S}
+    i1, j1 = node1
+    i2, j2 = node2
 
-    p21l = projector(network, (i, j, 2), (i, j+1, 1))
-    p22l = projector(network, (i, j, 2), (i, j+1, 2))
-    p12l = projector(network, (i, j, 1), (i, j+1, 2))
-    p11l = projector(network, (i, j, 1), (i, j+1, 1))
+    p21l = projector(network, (i1, j1, 2), (i2, j2, 1))
+    p22l = projector(network, (i1, j1, 2), (i2, j2, 2))
+    p12l = projector(network, (i1, j1, 1), (i2, j2, 2))
+    p11l = projector(network, (i1, j1, 1), (i2, j2, 1))
 
     p1l, (p11l, p12l) = fuse_projectors((p11l, p12l))
     p2l, (p21l, p22l) = fuse_projectors((p21l, p22l))
 
-    e11 = interaction_energy(network, (i, j, 1), (i, j+1, 1))
-    e12 = interaction_energy(network, (i, j, 1), (i, j+1, 2))
-    e21 = interaction_energy(network, (i, j, 2), (i, j+1, 1))
-    e22 = interaction_energy(network, (i, j, 2), (i, j+1, 2))
+    p11r = projector(network, (i2, j2, 1), (i1, j1, 1))
+    p21r = projector(network, (i2, j2, 1), (i1, j1, 2))
+    p12r = projector(network, (i2, j2, 2), (i1, j1 ,1))
+    p22r = projector(network, (i2, j2, 2), (i1, j1, 2))
+
+    p1r, (p11r, p21r) = fuse_projectors((p11r, p21r))
+    p2r, (p12r, p22r) = fuse_projectors((p12r, p22r))
+
+    e11 = interaction_energy(network, (i1, j1, 1), (i2, j2, 1))
+    e12 = interaction_energy(network, (i1, j1, 1), (i2, j2, 2))
+    e21 = interaction_energy(network, (i1, j1, 2), (i2, j2, 1))
+    e22 = interaction_energy(network, (i1, j1, 2), (i2, j2, 2))
 
     e11 = e11[p11l, p11r]
     e21 = e21[p21l, p21r]
@@ -427,13 +380,13 @@ function tensor(
     le22 = exp.(-β .* (e22 .- minimum(e22)))
 
     sl = maximum(outer_projector(
-        projector(network, (i, j, 1), ((i, j+1, 1), (i, j+1, 2))),  # r
-        projector(network, (i, j, 2), ((i, j+1, 1), (i, j+1, 2))),  # r
+        projector(network, (i1, j1, 1), ((i2, j2, 1), (i2, j2, 2))),  # r
+        projector(network, (i1, j1, 2), ((i2, j2, 1), (i2, j2, 2))),  # r
     ))
 
     sr = maximum(outer_projector(
-            projector(network, (i, j+1, 1), ((i, j, 1), (i, j, 2))),  # l
-            projector(network, (i, j+1, 2), ((i, j, 1), (i, j, 2))),  # l
+            projector(network, (i2, j2, 1), ((i1, j1, 1), (i1, j1, 2))),  # l
+            projector(network, (i2, j2, 2), ((i1, j1, 1), (i1, j1, 2))),  # l
         ))
     SparseCentralTensor(
         le11,
@@ -442,7 +395,8 @@ function tensor(
         le22,
         (sl, sr)
     )
-end
+end 
+
 
 """
 $(TYPEDSIGNATURES)
@@ -494,54 +448,54 @@ function tensor(
     network::PEPSNetwork{Square2{S}, T}, node::PEPSNode, β::Real, ::Val{:central_v}
 ) where {T <: AbstractSparsity, S <: AbstractTensorsLayout}
     i, j = floor(Int, node.i), node.j
+    getSparseCentralTensor(network, β, (i, j), (i+1, j))
+    # p11u = projector(network, (i+1, j, 1), (i, j, 1))
+    # p12u = projector(network, (i+1, j, 2), (i, j, 1))
+    # p21u = projector(network, (i+1, j, 1), (i, j, 2))
+    # p22u = projector(network, (i+1, j, 2), (i, j, 2))
 
-    p11u = projector(network, (i+1, j, 1), (i, j, 1))
-    p12u = projector(network, (i+1, j, 2), (i, j, 1))
-    p21u = projector(network, (i+1, j, 1), (i, j, 2))
-    p22u = projector(network, (i+1, j, 2), (i, j, 2))
+    # p1u, (p11u, p21u) = fuse_projectors((p11u, p21u))
+    # p2u, (p12u, p22u) = fuse_projectors((p12u, p22u))
 
-    p1u, (p11u, p21u) = fuse_projectors((p11u, p21u))
-    p2u, (p12u, p22u) = fuse_projectors((p12u, p22u))
+    # p11d = projector(network, (i, j, 1), (i+1, j, 1))
+    # p12d = projector(network, (i, j, 1), (i+1, j, 2))
+    # p21d = projector(network, (i, j, 2), (i+1, j, 1))
+    # p22d = projector(network, (i, j, 2), (i+1, j, 2))
 
-    p11d = projector(network, (i, j, 1), (i+1, j, 1))
-    p12d = projector(network, (i, j, 1), (i+1, j, 2))
-    p21d = projector(network, (i, j, 2), (i+1, j, 1))
-    p22d = projector(network, (i, j, 2), (i+1, j, 2))
+    # p1d, (p11d, p12d) = fuse_projectors((p11d, p12d))
+    # p2d, (p21d, p22d) = fuse_projectors((p21d, p22d))
 
-    p1d, (p11d, p12d) = fuse_projectors((p11d, p12d))
-    p2d, (p21d, p22d) = fuse_projectors((p21d, p22d))
+    # e11 = interaction_energy(network, (i, j, 1), (i+1, j, 1))
+    # e21 = interaction_energy(network, (i, j, 2), (i+1, j, 1))
+    # e12 = interaction_energy(network, (i, j, 1), (i+1, j, 2))
+    # e22 = interaction_energy(network, (i, j, 2), (i+1, j, 2))
 
-    e11 = interaction_energy(network, (i, j, 1), (i+1, j, 1))
-    e21 = interaction_energy(network, (i, j, 2), (i+1, j, 1))
-    e12 = interaction_energy(network, (i, j, 1), (i+1, j, 2))
-    e22 = interaction_energy(network, (i, j, 2), (i+1, j, 2))
+    # e11 = e11[p11d, p11u]
+    # e21 = e21[p21d, p21u]
+    # e12 = e12[p12d, p12u]
+    # e22 = e22[p22d, p22u]
 
-    e11 = e11[p11d, p11u]
-    e21 = e21[p21d, p21u]
-    e12 = e12[p12d, p12u]
-    e22 = e22[p22d, p22u]
+    # le11 = exp.(-β .* (e11 .- minimum(e11)))
+    # le21 = exp.(-β .* (e21 .- minimum(e21)))
+    # le12 = exp.(-β .* (e12 .- minimum(e12)))
+    # le22 = exp.(-β .* (e22 .- minimum(e22)))
 
-    le11 = exp.(-β .* (e11 .- minimum(e11)))
-    le21 = exp.(-β .* (e21 .- minimum(e21)))
-    le12 = exp.(-β .* (e12 .- minimum(e12)))
-    le22 = exp.(-β .* (e22 .- minimum(e22)))
+    # su = maximum(outer_projector(
+    #     projector(network, (i, j, 1), ((i+1, j, 1), (i+1, j, 2))),  # b
+    #     projector(network, (i, j, 2), ((i+1, j, 1), (i+1, j, 2))),  # b
+    # ))
+    # sd = maximum(outer_projector(
+    #     projector(network, (i+1, j, 1), ((i, j, 1), (i, j, 2))),  # t
+    #     projector(network, (i+1, j, 2), ((i, j, 1), (i, j, 2))),  # t
+    # ))
 
-    su = maximum(outer_projector(
-        projector(network, (i, j, 1), ((i+1, j, 1), (i+1, j, 2))),  # b
-        projector(network, (i, j, 2), ((i+1, j, 1), (i+1, j, 2))),  # b
-    ))
-    sd = maximum(outer_projector(
-        projector(network, (i+1, j, 1), ((i, j, 1), (i, j, 2))),  # t
-        projector(network, (i+1, j, 2), ((i, j, 1), (i, j, 2))),  # t
-    ))
-
-    SparseCentralTensor(
-        le11,
-        le12,
-        le21,
-        le22,
-        (su, sd)
-    )
+    # SparseCentralTensor(
+    #     le11,
+    #     le12,
+    #     le21,
+    #     le22,
+    #     (su, sd)
+    # )
 end
 
 """
---
+++ src/square_star2.jl
@@ -0,0 +1,373 @@
+export SquareStar2 # update_reduced_env_right
+
+struct SquareStar2{T <: AbstractTensorsLayout} <: AbstractGeometry end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function SquareStar2(m::Int, n::Int)
+    lg = Square2(m, n)
+    for i ∈ 1:m-1, j ∈ 1:n-1
+        add_edge!(lg, (i, j, 1), (i+1, j+1, 1))
+        add_edge!(lg, (i, j, 1), (i+1, j+1, 2))
+        add_edge!(lg, (i, j, 2), (i+1, j+1, 1))
+        add_edge!(lg, (i, j, 2), (i+1, j+1, 2))
+        add_edge!(lg, (i+1, j, 1), (i, j+1, 1))
+        add_edge!(lg, (i+1, j, 1), (i, j+1, 2))
+        add_edge!(lg, (i+1, j, 2), (i, j+1, 1))
+        add_edge!(lg, (i+1, j, 2), (i, j+1, 2))
+    end
+    lg
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+Virtual2(::Type{Dense}) = :virtual2
+
+"""
+$(TYPEDSIGNATURES)
+"""
+Virtual2(::Type{Sparse}) = :sparse_virtual2
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function tensor_map(
+    ::Type{SquareStar2{T}}, ::Type{S}, nrows::Int, ncols::Int
+) where {T <: Union{EnergyGauges, GaugesEnergy}, S <: AbstractSparsity}
+    map = Dict{PEPSNode, Symbol}()
+
+    for i ∈ 1:nrows, j ∈ 1:ncols
+        push!(
+            map,
+            PEPSNode(i, j) => site2(S),
+            PEPSNode(i, j - 1//2) => Virtual2(S),
+            PEPSNode(i + 1//2, j) => :central_v
+        )
+    end
+
+    for i ∈ 1:nrows-1, j ∈ 0:ncols-1   # why from 0?
+        push!(map, PEPSNode(i + 1//2, j + 1//2) => :central_d2)
+    end
+    map
+end
+
+# """
+# $(TYPEDSIGNATURES)
+# """
+# function tensor_map(
+#     ::Type{SquareStar2{T}}, ::Type{S}, nrows::Int, ncols::Int
+# ) where {T <: EngGaugesEng, S <: AbstractSparsity}
+#     map = Dict{PEPSNode, Symbol}()
+
+#     for i ∈ 1:nrows, j ∈ 1:ncols
+#         push!(
+#             map,
+#             PEPSNode(i, j) => site2(S),
+#             PEPSNode(i, j - 1//2) => Virtual2(S),
+#             PEPSNode(i + 1//5, j) => :sqrt_up,
+#             PEPSNode(i + 4//5, j) => :sqrt_down
+#         )
+#     end
+
+#     for i ∈ 1:nrows-1, j ∈ 0:ncols-1
+#         push!(
+#             map,
+#             PEPSNode(i + 1//5, j + 1//2) => :sqrt_up_d,
+#             PEPSNode(i + 4//5, j + 1//2) => :sqrt_down_d
+#         )
+#     end
+#     map
+# end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function gauges_list(::Type{SquareStar2{T}}, nrows::Int, ncols::Int) where T <: GaugesEnergy
+    [
+        GaugeInfo(
+            (PEPSNode(i + 1//6, j), PEPSNode(i + 2//6, j)),
+            PEPSNode(i + 1//2, j),
+            1,
+            :gauge_h
+        )
+        for i ∈ 1:nrows-1 for j ∈ 1//2:1//2:ncols
+    ]
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function gauges_list(::Type{SquareStar2{T}}, nrows::Int, ncols::Int) where T <: EnergyGauges
+    [
+        GaugeInfo(
+            (PEPSNode(i + 4//6, j), PEPSNode(i + 5//6, j)),
+            PEPSNode(i + 1//2, j),
+            2,
+            :gauge_h
+        )
+        for i ∈ 1:nrows-1 for j ∈ 1//2:1//2:ncols
+    ]
+end
+
+# """
+# $(TYPEDSIGNATURES)
+# """
+# function gauges_list(::Type{SquareStar2{T}}, nrows::Int, ncols::Int) where T <: EngGaugesEng
+#     [
+#         GaugeInfo(
+#             (PEPSNode(i + 2//5, j), PEPSNode(i + 3//5, j)),
+#             PEPSNode(i + 1//5, j),
+#             2,
+#             :gauge_h
+#         )
+#         for i ∈ 1:nrows-1 for j ∈ 1//2:1//2:ncols
+#     ]
+# end
+
+"""
+$(TYPEDSIGNATURES)
+
+Defines the MPO layers for the SquareStar2 geometry with the EnergyGauges layout.
+"""
+function MpoLayers(::Type{T}, ncols::Int) where T <: SquareStar2{EnergyGauges}
+    MpoLayers(
+        # Dict(site(i) => (-1//6, 0, 3//6, 4//6) for i ∈ 1//2:1//2:ncols),  # for now removes gauges
+        # Dict(site(i) => (3//6, 4//6) for i ∈ 1//2:1//2:ncols),
+        Dict(site(i) => (0, 3//6) for i ∈ 1//2:1//2:ncols),
+        Dict(site(i) => (3//6,) for i ∈ 1//2:1//2:ncols),
+        Dict(site(i) => (-3//6, 0) for i ∈ 1//2:1//2:ncols)
+    )
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+Defines the MPO layers for the SquareStar2 geometry with the GaugesEnergy layout.
+"""
+function MpoLayers(::Type{T}, ncols::Int) where T <: SquareStar2{GaugesEnergy}
+    MpoLayers(
+        # Dict(site(i) => (-4//6, -1//2, 0, 1//6) for i ∈ 1//2:1//2:ncols),
+        # Dict(site(i) => (1//6,) for i ∈ 1//2:1//2:ncols),
+        Dict(site(i) => (-1//2, 0,) for i ∈ 1//2:1//2:ncols),
+        Dict(site(i) => () for i ∈ 1//2:1//2:ncols),
+        Dict(site(i) => (-3//6, 0) for i ∈ 1//2:1//2:ncols)
+    )
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+# Defines the MPO layers for the SquareStar2 geometry with the EngGaugesEng layout.
+# """
+# function MpoLayers(::Type{T}, ncols::Int) where T <: SquareStar2{EngGaugesEng}
+#     MpoLayers(
+#         Dict(site(i) => (-2//5, -1//5, 0, 1//5, 2//5) for i ∈ 1//2:1//2:ncols),
+#         Dict(site(i) => (1//5, 2//5) for i ∈ 1//2:1//2:ncols),
+#         Dict(site(i) => (-4//5, -1//5, 0) for i ∈ 1//2:1//2:ncols)
+#     )
+# end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+# TODO: rewrite this using brodcasting if possible
+function conditional_probability(  # TODO
+    ::Type{T}, ctr::MpsContractor{S}, ∂v::Vector{Int}
+) where {T <: SquareStar2, S}
+    indβ, β = length(ctr.betas), last(ctr.betas)
+    i, j = ctr.current_node
+
+    L = left_env(ctr, i, ∂v[1:2*j-2], indβ)
+    R = right_env(ctr, i, ∂v[(2*j+3):2*ctr.peps.ncols+2], indβ)
+    ψ = dressed_mps(ctr, i, indβ)
+
+    MX, M = ψ[j-1//2], ψ[j]
+    @tensor LMX[y, z] := L[x] * MX[x, y, z]
+
+    v = ((i, j-1), (i-1, j-1), (i-1, j))
+    @nexprs 3 k->(
+        en_k = projected_energy(ctr.peps, (i, j), v[k], ∂v[2*j-1+k]);
+    )
+    probs = probability(local_energy(ctr.peps, (i, j)) .+ en_1 .+ en_2 .+ en_3, β)
+
+    p_rb = projector(ctr.peps, (i, j), (i+1, j-1))
+    pr = projector(ctr.peps, (i, j), @ntuple 3 k->(i+2-k, j+1))
+    pd = projector(ctr.peps, (i, j), (i+1, j))
+
+    @cast lmx2[b, c, d] := LMX[(b, c), d] (c ∈ 1:maximum(p_rb))
+
+    for σ ∈ 1:length(probs)
+        lmx = @inbounds lmx2[∂v[2*j-1], p_rb[σ], :]
+        m = @inbounds M[:, pd[σ], :]
+        r = @inbounds R[:, pr[σ]]
+        @inbounds probs[σ] *= (lmx' * m * r)[]
+    end
+
+    push!(ctr.statistics, ((i, j), ∂v) => error_measure(probs))
+    normalize_probability(probs)
+end
+
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function update_reduced_env_right(  # TODO
+    K::Array{T, 1},
+    RE::Array{T, 2},
+    M::SparseVirtualTensor2,
+    B::Array{T, 3}
+) where T <: Real
+    h = M.con
+    p_lb, p_l, p_lt, p_rb, p_r, p_rt = M.projs
+    @cast B4[x, k, l, y] := B[x, (k, l), y] (k ∈ 1:maximum(p_lb))
+    @cast K2[t1, t2] := K[(t1, t2)] (t1 ∈ 1:maximum(p_rt))
+    @tensor REB[x, y1, y2, β] := B4[x, y1, y2, α] * RE[α, β]
+    R = zeros(size(B, 1), length(p_l))
+    for l ∈ 1:length(p_l), r ∈ 1:length(p_r)
+        @inbounds R[:, l] += (K2[p_rt[r], p_lt[l]] .* h[p_l[l], p_r[r]]) .*
+                              REB[:, p_lb[l], p_rb[r], r]
+    end
+    R
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function nodes_search_order_Mps(peps::PEPSNetwork{T, S}) where {T <: SquareStar2, S}
+    ([(i, j, k) for i ∈ 1:peps.nrows for j ∈ 1:peps.ncols for k ∈ 1:2], (peps.nrows+1, 1, 1))
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function boundary(::Type{T}, ctr::MpsContractor{S}, node::Node) where {T <: SquareStar2, S}
+    i, j = node  # todo
+    vcat(
+        [
+            [((i, k-1), (i+1, k), (i, k), (i+1, k-1)), ((i, k), (i+1, k))]
+            for k ∈ 1:(j-1)
+        ]...,
+        ((i, j-1), (i+1, j)),
+        ((i, j-1), (i, j)),
+        ((i-1, j-1), (i, j)),
+        ((i-1, j), (i, j)),
+        [
+            [((i-1, k-1), (i, k), (i-1, k), (i, k-1)), ((i-1, k), (i, k))]
+            for k ∈ (j+1):ctr.peps.ncols
+        ]...
+    )
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function update_energy(
+    ::Type{T}, ctr::MpsContractor{S}, σ::Vector{Int}
+) where {T <: SquareStar2, S}
+    net = ctr.peps
+    i, j, k = ctr.current_node
+
+    en = local_energy(net, (i, j, k))
+    for v ∈ ((i, j-1, 1), (i, j-1, 2), (i-1, j, 1), (i-1, j, 2), (i-1, j-1, 1), (i-1, j-1, 2), (i-1, j+1, 1), (i-1, j+1, 2))
+        en += bond_energy(net, (i, j), v, local_state_for_node(ctr, σ, v))
+    end
+    if k != 2 return en end
+    en += bond_energy(net, (i, j, k), (i, j, 1), local_state_for_node(ctr, σ, (i, j, 1)))  # here k=2
+    en
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function tensor(  #TODO
+    net::PEPSNetwork{SquareStar2{T}, S}, node::PEPSNode, β::Real, ::Val{:central_d2}
+) where {T <: AbstractTensorsLayout, S <: AbstractSparsity}
+    i, j = floor(Int, node.i), floor(Int, node.j)
+    @nexprs 2 k -> T_k = connecting_tensor(net, (i, j-1+k), (i+1, j+2-k), β) # NE, NE
+    @cast A[(u, uu), (d, dd)] := T_1[u, d] * T_2[uu, dd]
+    A
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function Base.size(  #TODO
+    network::PEPSNetwork{SquareStar2{T}, S}, node::PEPSNode, ::Val{:central_d2}
+) where {T <: AbstractTensorsLayout, S <: AbstractSparsity}
+    i, j = floor(Int, node.i), floor(Int, node.j)
+    @nexprs 2 k -> s_k = size(interaction_energy(network, (i, j+k-1), (i + 1, j+2-k)))
+    (s_1[1] * s_2[1], s_1[2] * s_2[2])
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function tensor(  #TODO
+    net::PEPSNetwork{SquareStar2{T}, S}, node::PEPSNode, β::Real, ::Val{:sparse_virtual2}
+) where {T <: AbstractTensorsLayout, S <: Union{Sparse, Dense}}
+    v = Node(node)
+    i, j = node.i, floor(Int, node.j)
+
+    pl = last(fuse_projectors(
+        @ntuple 3 k->projector(net, (i, j), (i+2-k, j+1)) # p_lb, p_l, p_lt
+    ))
+    pr = last(fuse_projectors(
+        @ntuple 3 k->projector(net, (i, j+1), (i+2-k, j)) # p_rb, p_r, p_rt
+    ))
+    SparseVirtualTensor2(
+       connecting_tensor(net, floor.(Int, v), ceil.(Int, v), β),
+       vec.((pl..., pr...))
+    )
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function tensor(  #TODO
+    net::PEPSNetwork{SquareStar2{T}, Dense}, node::PEPSNode, β::Real, ::Val{:virtual2}
+) where T <: AbstractTensorsLayout
+    sp = tensor(net, node, β, Val(:sparse_virtual2))
+    p_lb, p_l, p_lt, p_rb, p_r, p_rt = sp.projs
+
+    A = zeros(
+        eltype(sp.con),
+        length(p_l), maximum.((p_rt, p_lt))..., length(p_r), maximum.((p_lb, p_rb))...
+    )
+    for l ∈ 1:length(p_l), r ∈ 1:length(p_r)
+        @inbounds A[l, p_rt[r], p_lt[l], r, p_lb[l], p_rb[r]] = sp.con[p_l[l], p_r[r]]
+    end
+    @cast B[l, (uu, u), r, (dd, d)] := A[l, uu, u, r, dd, d]
+    B
+end
+
+
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function projectors_site_tensor(
+    network::PEPSNetwork{T, S}, vertex::Node
+) where {T <: SquareStar2, S}
+    i, j = vertex
+    pl = outer_projector(
+        (projector(net, (i, j, k), ((i, j-1, 1), (i, j-1, 2))) for k ∈ 1:2)...)
+    pt = outer_projector(
+        (projector(net, (i, j, k), ((i-1, j, 1), (i-1, j, 2))) for k ∈ 1:2)...)
+    pr = outer_projector(
+        (projector(net, (i, j, k), ((i, j+1, 1), (i, j+1, 2))) for k ∈ 1:2)...)
+    pb = outer_projector(
+        (projector(net, (i, j, k), ((i+1, j, 1), (i+1, j, 2))) for k ∈ 1:2)...)
+    plb = outer_projector(
+        (projector(net, (i, j, k), ((i+1, j-1, 1), (i+1, j-1, 2))) for k ∈ 1:2)...)
+    plt = outer_projector(
+        (projector(net, (i, j, k), ((i-1, j-1, 1), (i-1, j-1, 2))) for k ∈ 1:2)...)
+    prb = outer_projector(
+        (projector(net, (i, j, k), ((i+1, j+1, 1), (i+1, j+1, 2))) for k ∈ 1:2)...)
+    prt = outer_projector(
+        (projector(net, (i, j, k), ((i-1, j+1, 1), (i-1, j+1, 2))) for k ∈ 1:2)...)
+    plf = first(fuse_projectors(plb, pl, plt))
+    prf = first(fuse_projectors(prb, pr, prt))
+    (plf, pt, prf, pb)
+end

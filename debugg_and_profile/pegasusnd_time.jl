9b5b4e5554ea5a9451a0de6e39771d55f3cb394d
tomsmierz <smierzchalski.tomek@gmail.com>
2022-08-25 14:23:14 +0200

    testing PegasusSquare

--- debugg_and_profile/pegasusnd_time.jl
+++ debugg_and_profile/pegasusnd_time.jl
@@ -7,7 +7,7 @@ using Profile, PProf
 using FlameGraphs
 
 disable_logging(LogLevel(1))
-Profile.init(n = 10^8, delay = 0.01)
+Profile.init(n = 10^10, delay = 0.01)
 
 function brute_force_gpu(ig::IsingGraph; num_states::Int)
     brute_force(ig, :GPU, num_states=num_states)
@@ -15,15 +15,16 @@ end
 
 
 function bench(instance::String)
-    m = 4
-    n = 4
+    m = 2
+    n = 2
     t = 3
 
     β = 3
     bond_dim = 4
     δp = 1e-10
     num_states = 10
-
+    println("creating factor graph" )
+    @time begin
     ig = ising_graph(instance)
 
     fg = factor_graph(
@@ -31,7 +32,7 @@ function bench(instance::String)
     spectrum=brute_force_gpu, # rm _gpu to use CPU
     cluster_assignment_rule=pegasus_lattice((m, n, t))
     )
-
+    end
     params = MpsParameters(bond_dim, 1E-2, 1)
     search_params = SearchParameters(num_states, δp)
 
@@ -42,14 +43,16 @@ function bench(instance::String)
     tran =  rotation(0)
     Layout = GaugesEnergy
     Gauge = NoUpdate
-
-    net = PEPSNetwork{SquareStar2{Layout}, Sparsity}(m, n, fg, tran)
+    println("creating network and contractor")
+    @time begin
+    net = PEPSNetwork{PegasusSquare, Sparsity}(m, n, fg, tran)
     ctr = MpsContractor{Strategy, Gauge}(net, [β], :graduate_truncate, params)
-
+    end
+    println("solving")
     @time sol = low_energy_spectrum(ctr, search_params, merge_branches(ctr))
 end
 
-instance = "$(@__DIR__)/../test/instances/pegasus_droplets/4_4_3_00.txt"
+instance = "$(@__DIR__)/../test/instances/pegasus_nondiag/pegasus_nd_2x2x3.txt"
 
 bench(instance)
 @profile bench(instance)

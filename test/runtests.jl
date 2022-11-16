ed16f63238c221257fe1ee020a5f145b7784d741
marekrams <marek.rams@uj.edu.pl>
2022-11-16 12:23:18 +0100

    cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,28 +40,28 @@ my_tests = []
 
 push!(my_tests,
 # quick tests:
-     "operations.jl",
-     "branch_and_bound.jl",
-     "search_chimera_pathological.jl",
-     "search_chimera_smallest.jl",
-     "search_cross_square_star.jl",
-     "search_smallest_cross_square_star.jl",
-     "search_pegasus_square_star.jl",
-     "search_pegasus_nodiag_square_star.jl",
-     "search_square2_basic.jl",
-     "search_squarestar2_basic.jl",
-     #"search_chimera_gauge.jl",
-     #"search_chimera_pathological_gauge.jl",
-
+    "operations.jl",
+    "branch_and_bound.jl",
+    "search_chimera_pathological.jl",
+    "search_chimera_smallest.jl",
+    "search_cross_square_star.jl",
+    "search_smallest_cross_square_star.jl",
+    "search_pegasus_square_star.jl",
+    "search_pegasus_nodiag_square_star.jl",
+    "search_square2_basic.jl",
+    "search_squarestar2_basic.jl",
+    "search_chimera_pathological_gauge.jl",
+    "chimera_overlap_python.jl",
+    
 # time consuming tests:
-    #"search_chimera_full.jl",
+    "search_chimera_full.jl",
+    "search_chimera_gauge.jl",
+    "gauges.jl"
 )
 
 # This is work in progress (may or may not be included in future versions)
 
 push!(my_tests,
-    #"experimental/chimera_overlap_python.jl", # OK
-    # "experimental/gauges.jl",
     # "experimental/squarestar2_pegasus.jl",
     # "experimental/squarestar2_zephyr.jl",
 )

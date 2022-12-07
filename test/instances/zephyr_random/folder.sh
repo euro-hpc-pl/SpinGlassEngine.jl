d04ff42d7e1bdc83c4c38bdfad5fb8fac1c54ac3
tomsmierz <smierzchalski.tomek@gmail.com>
2022-12-07 09:42:31 +0100

    zephyr instances

---
+++ test/instances/zephyr_random/folder.sh
@@ -0,0 +1,21 @@
+for size in 2 3 4
+do
+  for cat in RAU RCO 
+  do
+    cd "Z$size"
+    cd "$cat"
+	mkdir SpinGlass
+	mkdir DWave
+	for f in ./*.txt
+	do
+		mv $f SpinGlass
+	done
+	for f in ./*.pkl
+	do
+		mv $f DWave
+	done
+
+	cd ..
+	cd ..
+  done
+done

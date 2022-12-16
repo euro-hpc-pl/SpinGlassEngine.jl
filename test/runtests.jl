5dcc624004a52dd562c3dc22587b9cc3d4eeb188
bartekGardas <bartek.gardas@gmail.com>
2022-12-16 14:59:54 +0100

    rm AbstracDitcs

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,37 +12,13 @@ disable_logging(LogLevel(1))
 
 using Test
 
-#=
-function proj(state, dims::Union{Vector, NTuple})
-    P = Matrix{Float64}[]
-    for (σ, r) ∈ zip(state, dims)
-        v = zeros(r)
-        v[idx(σ)...] = 1.
-        push!(P, v * v')
-    end
-    P
-end
-
-function SpinGlassEngine.tensor(ψ::AbstractMPS, state::State)
-    C = I
-    for (A, σ) ∈ zip(ψ, state) C *= A[:, idx(σ), :] end
-    tr(C)
-end
-
-# function SpinGlassEngine.tensor(ψ::MPS)
-#     dims = rank(ψ)
-#     Θ = Array{eltype(ψ)}(undef, dims)
-#     for σ ∈ all_states(dims) Θ[idx.(σ)...] = tensor(ψ, σ) end
-#     Θ
-# end
-=#
 using Test
 my_tests = []
 
 push!(my_tests,
 # quick tests:
-    #"operations.jl",
-    #"branch_and_bound.jl",
+    "operations.jl",
+    "branch_and_bound.jl",
     "search_chimera_pathological.jl",
     "search_chimera_smallest.jl",
     "search_cross_square_star.jl",

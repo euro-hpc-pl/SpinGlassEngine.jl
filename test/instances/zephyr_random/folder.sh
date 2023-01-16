0416498dced8e8fe9a66005dc5fb5b352820d842
tomsmierz <smierzchalski.tomek@gmail.com>
2023-01-16 14:22:09 +0100

    added AC3 instances for zephyr

--- test/instances/zephyr_random/folder.sh
+++ test/instances/zephyr_random/folder.sh
@@ -1,21 +1,24 @@
 for size in 2 3 4
 do
-  for cat in RAU RCO 
-  do
-    cd "Z$size"
-    cd "$cat"
-	mkdir SpinGlass
-	mkdir DWave
-	for f in ./*.txt
-	do
-		mv $f SpinGlass
-	done
-	for f in ./*.pkl
-	do
-		mv $f DWave
-	done
+	for cat in RAU RCO AC3
+  	do
+    	cd "Z$size"
+    	cd "$cat"
 
-	cd ..
-	cd ..
-  done
+		mkdir SpinGlass
+		mkdir DWave
+
+		for f in ./*.txt
+		do
+			mv $f SpinGlass
+		done
+
+		for f in ./*.pkl
+		do
+			mv $f DWave
+		done
+
+		cd ..
+		cd ..
+	done
 done

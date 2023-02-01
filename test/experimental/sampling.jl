95f7c97cf3633a9436fbaf0111a4c3b211fdc0ed
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2023-02-01 17:19:30 +0100

    overlap between states in sampling

--- test/experimental/sampling.jl
+++ test/experimental/sampling.jl
@@ -4,13 +4,21 @@ function brute_force_gpu(ig::IsingGraph; num_states::Int)
      brute_force(ig, :GPU, num_states=num_states)
 end
 
-m, n, t = 4, 4, 1
+function overlap_states(state1::Vector{Int}, state2::Vector{Int})
+    s1 = reshape(state1, :, length(state1))
+    s2 = reshape(state2, :, length(state2))
+    n1 = sqrt(dot(s1, s1))
+    n2 = sqrt(dot(s2, s2))
+    dot(s1, s2) / (n1 * n2)
+end
+
+m, n, t = 12, 12, 1
 
 β = 1.0
 bond_dim = 2
 num_states = 7 #22
 
-ig = ising_graph("$(@__DIR__)/../instances/square_gauss/S4/001.txt")
+ig = ising_graph("$(@__DIR__)/../instances/square_gauss/S12/001.txt")
 fg = factor_graph(
     ig,
     spectrum=brute_force_gpu,
@@ -31,5 +39,10 @@ net = PEPSNetwork{Lattice{Layout}, Sparsity}(m, n, fg, transform)
 ctr = MpsContractor{Strategy, Gauge}(net, [β/8., β/4., β/2., β], :graduate_truncate, params)
 sol = gibbs_sampling(ctr, search_params)
 
+for i in 1:num_states-1
+    o = overlap_states(sol.states[i], sol.states[i+1])
+    println("Overlap between states ", i, " and ", i+1, " is ", o)
+end
+
 println(sol.energies)
-println(sol.states)
+#println(sol.states)

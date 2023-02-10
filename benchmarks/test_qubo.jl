6576650303852de5275e943b8f863045dbaab100
Bartek Gardas <bartek.gardas@gmail.com>
2023-02-10 16:17:39 +0100

    fix a bug

--- benchmarks/test_qubo.jl
+++ benchmarks/test_qubo.jl
@@ -1,7 +1,7 @@
 using LinearAlgebra
 
 function qubo(h::Vector, J::Matrix)
-    b = dropdims(sum(J, dims=2), dims=2)
+    b = [sum(J[i, :]) + sum(J[:, i]) for i ∈ 1:length(h)]
     4 .* J .+ 2 .* Diagonal(h .- b)
 end
 
@@ -10,14 +10,11 @@ qubo_energy(Q::Matrix, σ::Vector) = σ' * Q * σ
 qubo_state(s::Vector) = (s .+ 1) ./ 2
 offset(h::Vector, J::Matrix) = sum(J) - sum(h)
 
-n = 5
+n = 100
 T = Float64
 J = triu(rand(T, n, n), 1)
 h = rand(T, n)
 ising_state = [rand() < 0.5 ? -1 : 1 for _ ∈ 1:n]
 
-
 Q = qubo(h, J)
-#@assert ising_energy(h, J, ising_state) ≈ qubo_energy(Q, qubo_state(ising_state)) + offset(h, J)
-println(ising_energy(h, J, ising_state))
-qubo_energy(Q, qubo_state(ising_state)) + offset(h, J)
+@assert ising_energy(h, J, ising_state) ≈ qubo_energy(Q, qubo_state(ising_state)) + offset(h, J)

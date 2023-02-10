0ceec94147f28c539a74fa6e8b7b25baee7cb126
Bartek Gardas <bartek.gardas@gmail.com>
2023-02-10 12:27:48 +0100

    hunting for a bug

--- benchmarks/cplex_bench_ref.jl
+++ benchmarks/cplex_bench_ref.jl
@@ -4,8 +4,8 @@ using SpinGlassNetworks, LightGraphs
 
 qubo(h::Vector, J::Matrix) = 4 .* J .+ 2 .* Diagonal(h .- sum(J))
 
-instance = "$(@__DIR__)/../test/instances/pegasus_random/P4/CBFM-P/001_sg.txt"
-#instance = Dict((1, 2) => -1.0)
+#instance = "$(@__DIR__)/../test/instances/pegasus_random/P4/CBFM-P/001_sg.txt"
+instance = Dict((1, 2) => 1.0, (1, 3) => 1.0)
 ig = ising_graph(instance)
 h = biases(ig)
 J = couplings(ig)
@@ -18,3 +18,13 @@ optimize!(model)
 
 σ = value.(x)
 E = σ' * Q * σ + sum(J) - sum(h)
+println(σ)
+println(E)
+
+sp = brute_force(ig, :CPU; num_states=1)
+en = sp.energies[1]
+st = sp.states[1]
+σ2 = (st .+ 1) ./ 2
+println(σ2)
+println(en)
+@assert en ≈ σ2' * Q * σ2 + sum(J) - sum(h)

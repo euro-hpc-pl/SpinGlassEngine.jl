9ef191a7328af328e36f1e9426af015f589c98b6
tomsmierz <smierzchalski.tomek@gmail.com>
2023-03-06 16:49:37 +0100

    slicing bench

---
+++ benchmarks/slicing_bench.jl
@@ -0,0 +1,24 @@
+using BenchmarkTools, LinearAlgebra
+import SpinGlassTensors: format_bytes
+
+X = rand(Float64, (1000, 1000, 1000))
+X1p = Array{Float64}(undef, (1000, 1000, 300))
+X2p = Array{Float64}(undef, (1000, 1000, 300))
+p = sort(rand(1:50, 1000))
+vp = @view p[1:300]
+
+#using normal slicing always creates a copy, even when using prealocated memory
+a = @allocated X1p = X[:, :, vp]
+println("allocation using normal slicing: ", format_bytes(a))
+
+#using view doesn't create copy
+b = @allocated copy!(X2p, @view X[:, :, vp])
+println("allocation using @view: ", format_bytes(b))
+
+
+#changes to original tensor don't translate into allocated
+println("original X: ",  X[1:2,1:2,1])
+X[1:2,1:2,1] = [100 100; 100 100]
+println("changed X: ", X[1:2,1:2,1])
+println("X preallocated: ", X2p[1:2,1:2,1])
+

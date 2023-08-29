423eae4ac095dfd1af9d4a3e71c959022c119b31
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2023-08-29 08:17:35 +0200

    droplets add tests

--- test/hamming.jl
+++ test/hamming.jl
@@ -1,9 +1,8 @@
 using Test
+using SpinGlassEngine
+using SpinGlassNetworks
+using SpinGlassTensors
 
-
-function hamming_distance(dstate::Vector{Int})
-    sum(count_ones(st) for st in dstate)
-end
 
 @testset "count_ones() method" begin
 # Julia program to illustrate the use of count_ones() method
@@ -11,12 +10,6 @@ end
     for t in [0, 1, 15, 20]
         @test count_ones(t) == count(==('1'), bitstring(t))
     end
-end
-
-@testset "Hamming Distance Calculation" begin
-    @test hamming_distance([10, 6, 12]) == 6
-    @test hamming_distance([15, 0, 9]) == 6
-    @test hamming_distance([13, 13, 13]) == 9
 end
 
 @testset "XOR Operation Tests" begin
@@ -35,4 +28,20 @@ end
     integer_value = parse(Int, binary_string, base=2)
     println(integer_value)
     @test 10 ⊻ 5 == xor(10, 5) == 15 
+end
+
+flip1 = Flip([1,3,4,5], [2,3,5,6], [1, 1, 0, 1])
+flip2 = Flip([1,3,4,5], [1,2,3,4], [1, 1, 0, 1])
+flip3 = Flip([1,3,4,5], [2,3,5,6], [1, 1, 0, 0])
+
+@testset "Hamming Distance Calculation" begin
+    @test hamming_distance(flip1) == 3
+    @test hamming_distance(flip2) == 3
+    @test hamming_distance(flip3) == 2
+end
+
+@testset "Hamming distance between two droplets" begin
+    @test hamming_distance(flip1, flip2) == 0
+    @test hamming_distance(flip2, flip3) == 1
+
 end

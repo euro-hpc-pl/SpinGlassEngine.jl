45825cb690472996641e2959038b930304bb971f
annamariadziubyna <73058800+annamariadziubyna@users.noreply.github.com>
2023-10-03 14:15:42 +0200

    add docs for operations and contractor

--- src/operations.jl
+++ src/operations.jl
@@ -3,16 +3,28 @@ export
        rotation,
        reflection,
        all_lattice_transformations,
-       vertex_map
+       vertex_map,
+       check_bounds
 
+"""
+$(TYPEDSIGNATURES)
+
+A struct representing a lattice transformation.
+
+# Fields
+- `permutation::NTuple{4, Int}`: A tuple defining a permutation of the vertex labels.
+- `flips_dimensions::Bool`: A boolean indicating whether dimension flips are applied.
+
+The `LatticeTransformation` struct defines a transformation that can be applied to the vertices of a lattice. It specifies a permutation of vertex labels, allowing for rotations and reflections, as well as an option to flip dimensions. This struct is commonly used to describe various symmetry operations on lattice structures.
+"""
 struct LatticeTransformation
     permutation::NTuple{4, Int}
     flips_dimensions::Bool
 end
 
-"""
-$(TYPEDSIGNATURES)
-"""
+# """
+# $(TYPEDSIGNATURES)
+# """
 function Base.:(∘)(op1::LatticeTransformation, op2::LatticeTransformation)
     LatticeTransformation(
         op1.permutation[collect(op2.permutation)],
@@ -22,6 +34,17 @@ end
 
 """
 $(TYPEDSIGNATURES)
+Create a reflection transformation along the specified axis.
+
+# Arguments
+- `axis::Symbol`: The axis of reflection, which can be one of the following:
+  - `:x` for reflection along the x-axis.
+  - `:y` for reflection along the y-axis.
+  - `:diag` for reflection along the main diagonal.
+  - `:antydiag` for reflection along the anti-diagonal.
+
+# Returns
+A `LatticeTransformation` object representing the specified reflection transformation.
 """
 function reflection(axis::Symbol)
     if axis == :x
@@ -44,6 +67,13 @@ end
 
 """
 $(TYPEDSIGNATURES)
+Create a rotation transformation by a specified angle.
+
+# Arguments
+- `θ::Int`: The angle of rotation, expressed in degrees. Only integral multiples of 90° can be passed as θ.
+
+# Returns
+A `LatticeTransformation` object representing the specified rotation transformation.
 """
 function rotation(θ::Int)
     if θ % 90 != 0
@@ -61,6 +91,14 @@ end
 
 """
 $(TYPEDSIGNATURES)
+Create a bounds-checking function for a lattice of size (m, n).
+
+# Arguments
+- `m::Int`: The number of rows in the lattice.
+- `n::Int`: The number of columns in the lattice.
+
+# Returns
+A bounds-checking function that can be used to ensure that lattice points are within the specified bounds.
 """
 function check_bounds(m, n)
     function _check(i, j)
@@ -73,6 +111,15 @@ end
 
 """
 $(TYPEDSIGNATURES)
+Create a vertex mapping function for a lattice transformation.
+
+# Arguments
+- `vert_permutation::NTuple{4, Int}`: A permutation of vertex labels, defining a specific lattice transformation.
+- `nrows::Int`: The number of rows in the lattice.
+- `ncols::Int`: The number of columns in the lattice.
+    
+# Returns
+A vertex mapping function `vmap` that takes a tuple of vertex coordinates and returns their new coordinates after applying the specified lattice transformation.    
 """
 function vertex_map(vert_permutation::NTuple{4, Int}, nrows, ncols)
     if vert_permutation == (1, 2, 3, 4) #
@@ -101,6 +148,17 @@ end
 
 """
 $(TYPEDSIGNATURES)
+Create a vertex map function based on a given lattice transformation.
+
+This function generates a vertex map function that can be used to transform lattice vertex coordinates according to a specified lattice transformation. The `trans` argument should be a `LatticeTransformation` object, and `m` and `n` specify the dimensions of the lattice.
+    
+# Arguments
+- `trans::LatticeTransformation`: The lattice transformation to apply.
+- `m::Int`: The number of rows in the lattice.
+- `n::Int`: The number of columns in the lattice.
+    
+# Returns
+A vertex map function that takes vertex coordinates and returns the transformed coordinates.    
 """
 function vertex_map(trans::LatticeTransformation, m::Int, n::Int)
     vertex_map(trans.permutation, m, n)
@@ -108,6 +166,10 @@ end
 
 """
 $(TYPEDSIGNATURES)
+A tuple containing all possible lattice transformations.
+
+This constant includes rotations at angles 0, 90, 180, and 270 degrees, as well as reflections across the x-axis, y-axis, diagonal, and antidiagonal axes. 
+These lattice transformations can be applied to vertices of a lattice to achieve various orientations and reflections.
 """
 const all_lattice_transformations = (
     rotation.([0, 90, 180, 270])...,

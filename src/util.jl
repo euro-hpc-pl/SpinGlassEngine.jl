acd7ff1f685df8b985cf1b4c701bb6bb9b918453
tomsmierz <smierzchalski.tomek@gmail.com>
2023-11-08 15:11:42 +0100

    moved some functions from contractor.jl to util.jl

--- src/util.jl
+++ src/util.jl
@@ -1,8 +1,11 @@
 export
     exact_marginal_probability,
-    exact_conditional_probability
+    exact_conditional_probability,
+    conditional_probability,
+    update_energy
+    error_measure
 
-    """
+"""
 $(TYPEDSIGNATURES)
 """
 @memoize function exact_spectrum(clustered_hamiltonian::LabelledGraph{S, T}) where {S, T}  
@@ -33,4 +36,27 @@ function exact_conditional_probability(ctr::MpsContractor{T}, σ::Vector{Int}) w
     local_basis = collect(1:cluster_size(ctr.peps, ctr.current_node))
     probs = exact_marginal_probability.(Ref(ctr), branch_state(local_basis, σ))
     probs ./= sum(probs)
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function conditional_probability(ctr::MpsContractor{S}, w::Vector{Int}) where S
+    conditional_probability(layout(ctr.peps), ctr, w)
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function update_energy(ctr::MpsContractor{S}, w::Vector{Int}) where S
+    update_energy(layout(ctr.peps), ctr, w)
+end
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function error_measure(probs)
+    if maximum(probs) <= 0 return 2.0 end
+    if minimum(probs) < 0 return abs(minimum(probs)) / maximum(abs.(probs)) end
+    return 0.0
 end

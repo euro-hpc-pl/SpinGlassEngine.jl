ef064a3e7589a10487dae94de95df1f8debcb2d7
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2023-11-22 21:08:52 +0100

    fix the bug in rmf

--- test/random_markov_field.jl
+++ test/random_markov_field.jl
@@ -4,9 +4,9 @@
     β = 1.0
     bond_dim = 8
     δp = 1E-4
-    num_states = 10
+    num_states = 64
     cl_h = clustered_hamiltonian(instance_dir)
-    Nx, Ny = get_prop(cl_h, :Nx), get_prop(cl_h, Ny)
+    Nx, Ny = get_prop(cl_h, :Nx), get_prop(cl_h, :Ny)
     params = MpsParameters(bond_dim, 1E-8, 10)
     search_params = SearchParameters(num_states, δp)
     Gauge = NoUpdate
@@ -20,5 +20,5 @@
     ctr = MpsContractor{Strategy, Gauge}(net, [β/8, β/4, β/2, β], graduate_truncation, params; onGPU=onGPU)
     # sol_peps, s = low_energy_spectrum(ctr, search_params, merge_branches(ctr))
     sol_peps, s = low_energy_spectrum(ctr, search_params, merge_branches(ctr, :nofit, SingleLayerDroplets(100.0, 100, :hamming, :RMF)))
-    # println(sol_peps.energies[begin])
+    println(sol_peps.energies[begin])
 end

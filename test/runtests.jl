428e3fa1930679ec4584304c0218f534dd4bc77f
Łukasz Pawela <lukasz.pawela@gmail.com>
2024-03-22 16:43:23 +0100

    uncoment all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,29 +18,29 @@ my_tests = []
 
 push!(my_tests,
 # quick tests:
-    # "operations.jl",
-    # "branch_and_bound.jl",
-    # "search_chimera_pathological.jl",
-    # "search_chimera_smallest.jl",
-    # "search_cross_square_star.jl",
-    # "search_smallest_cross_square_cross.jl",
-    # "search_pegasus_square_cross.jl",
-    # "search_pegasus_nodiag_square_cross.jl",
-    # "search_square_double_node_basic.jl",
-    # "search_squarecross_double_node_basic.jl",
-    # "chimera_overlap_python.jl",
-    #"hamming.jl",
-    #"search_chimera_smallest_droplets.jl",
+    "operations.jl",
+    "branch_and_bound.jl",
+    "search_chimera_pathological.jl",
+    "search_chimera_smallest.jl",
+    "search_cross_square_star.jl",
+    "search_smallest_cross_square_cross.jl",
+    "search_pegasus_square_cross.jl",
+    "search_pegasus_nodiag_square_cross.jl",
+    "search_square_double_node_basic.jl",
+    "search_squarecross_double_node_basic.jl",
+    "chimera_overlap_python.jl",
+    "hamming.jl",
+    "search_chimera_smallest_droplets.jl",
     "search_chimera_pathological_droplets.jl",
     "search_chimera_pathological_hamming.jl",
-    # "search_chimera_droplets.jl",
-    # "search_pegasus_droplets.jl",
-    # "search_chimera_pathological_Z2.jl"
+    "search_chimera_droplets.jl",
+    "search_pegasus_droplets.jl",
+    "search_chimera_pathological_Z2.jl",
 
 # time consuming tests:
-    # "search_chimera_full.jl",
-    # "search_chimera_gauge.jl",
-    # "gauges.jl"
+    "search_chimera_full.jl",
+    "search_chimera_gauge.jl",
+    "gauges.jl"
 )
 
 # This is work in progress (may or may not be included in future versions)

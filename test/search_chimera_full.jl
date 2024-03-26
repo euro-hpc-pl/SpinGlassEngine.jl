4d892ed2a21ae14439eb4fbce4d34c3affa6fcd0
tomsmierz <smierzchalski.tomek@gmail.com>
2024-03-26 18:06:13 +0100

    search chimera full passes

--- test/search_chimera_full.jl
+++ test/search_chimera_full.jl
@@ -2,9 +2,6 @@ using SpinGlassNetworks
 using SpinGlassTensors
 using SpinGlassEngine
 
-function my_brute_force(ig::IsingGraph; num_states::Int)
-    brute_force(ig, onGPU ? :GPU : :CPU, num_states=num_states)
-end
 
 function bench(instance::String)
     m, n, t = 16, 16, 8
@@ -23,16 +20,16 @@ function bench(instance::String)
     cl_h = clustered_hamiltonian(
         ising_graph(instance),
         max_cl_states,
-        spectrum=my_brute_force,
+        spectrum=full_spectrum,
         cluster_assignment_rule=super_square_lattice((m, n, t))
     )
     params = MpsParameters(bond_dim, 1E-8, 10, 1E-16)
     search_params = SearchParameters(num_states, δp)
 
-    energies = Vector{Float64}[]
-    for Strategy ∈ (SVDTruncate, Zipper), Sparsity ∈ (Dense, )
+    for Strategy ∈ (SVDTruncate, Zipper), Sparsity ∈ (Dense, Sparse)
         for Gauge ∈ (NoUpdate, GaugeStrategy, GaugeStrategyWithBalancing)
             for Layout ∈ (GaugesEnergy,), transform ∈ all_lattice_transformations
+
                 net = PEPSNetwork{SquareSingleNode{Layout}, Sparsity}(m, n, cl_h, transform)
                 ctr = MpsContractor{Strategy, Gauge}(net, all_betas, :graduate_truncate, params; onGPU=onGPU)
                 sol, s = low_energy_spectrum(ctr, search_params, merge_branches(ctr, :nofit))
